1b3c1a38f941ae155c61769162f915be724ce6a7
David Carr <dave@daveismyname.com>
2014-04-13 21:00:11 +0100

    add 2nd option to redirect

--- app/helpers/url.php
+++ app/helpers/url.php
@@ -2,9 +2,16 @@
 
 class Url {
 
-	public static function redirect($url = null){
-		header('location: '.DIR.$url);
-		exit;
+	public static function redirect($url = null, $fullpath = false){
+		
+		if($fullpath == false){
+			header('location: '.DIR.$url);
+			exit;
+		} else {
+			header('location: '.$url);
+			exit;
+		}
+		
 	}
 
 	public static function get_template_path(){

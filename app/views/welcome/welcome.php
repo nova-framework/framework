e852641144fcba31e19e5f9391ffe90745ce95ce
Edwin Hoksberg <info@edwinhoksberg.nl>
2014-06-16 21:26:26 +0200

    Used data variable set in controller

--- app/views/welcome/welcome.php
+++ app/views/welcome/welcome.php
@@ -1,4 +1,4 @@
-<h1>Welcome</h1>
+<h1><?php echo $data['title']; ?></h1>
 <hr />
 <p>Hello, welcome from the welcome controller!</p>
 <p>This content can be changed in /app/views/welcome/welcome.php</p>

2a137c9fe1685f1c7de829fb1821abe8ceec936a
QsmaPL <QsmaPL@users.noreply.github.com>
2014-06-27 12:49:55 +0200

    Update footer.php

--- app/templates/default/footer.php
+++ app/templates/default/footer.php
@@ -3,11 +3,13 @@
 <script src="<?php echo helpers\url::get_template_path();?>js/jquery.js"></script>
 <?php echo $data['js']."\n";?>
 
+<?php if (isset($data['jq'])): ?>
 <script>
 $(document).ready(function(){
 	<?php echo $data['jq']."\n";?>
 });
 </script>
+<?php endif; ?>
 
 </body>
 </html>

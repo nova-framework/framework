b94ea13b517505e506d744970b39b871fc8ca6dd
Edwin Hoksberg <info@edwinhoksberg.nl>
2014-09-24 22:53:34 +0200

    Added safe slug function

--- app/helpers/url.php
+++ app/helpers/url.php
@@ -14,7 +14,7 @@ class Url {
 	 * @param  boolean $fullpath if true use only url in redirect instead of using DIR
 	 */
 	public static function redirect($url = null, $fullpath = false){
-		
+
 		if($fullpath == false){
 			header('location: '.DIR.$url);
 			exit;
@@ -22,12 +22,12 @@ class Url {
 			header('location: '.$url);
 			exit;
 		}
-		
+
 	}
 
 	/**
 	 * created the absolute address to the template folder
-	 * @return string url to template folder 
+	 * @return string url to template folder
 	 */
 	public static function get_template_path(){
 	    return DIR.'app/templates/'.Session::get('template').'/';
@@ -36,7 +36,7 @@ class Url {
 	/**
 	 * converts plain text urls into HTML links, second argument will be
 	 * used as the url label <a href=''>$custom</a>
-	 * 
+	 *
 	 * @param  string $text   data containing the text to read
 	 * @param  string $custom if provided, this is used for the link label
 	 * @return string         returns the data with links created around urls
@@ -46,9 +46,26 @@ class Url {
 			return preg_replace('@(http)?(s)?(://)?(([-\w]+\.)+([^\s]+)+[^,.\s])@', '<a href="http$2://$4">$1$2$3$4</a>', $text);
 		} else {
 			return preg_replace('@(http)?(s)?(://)?(([-\w]+\.)+([^\s]+)+[^,.\s])@', '<a href="http$2://$4">'.$custom.'</a>', $text);
-		}  
-	} 
-	
+		}
+	}
+
+	/**
+	 * This function converts and url segment to an safe one, for example:
+	 * `test name @132` will be converted to `test-name--123`
+	 * Basicly it works by replacing every character that isn't an letter or an number to an dash sign
+	 * It will also return all letters in lowercase
+	 *
+	 * @param $slug - The url slug to convert
+	 *
+	 * @return mixed|string
+	 */
+	public static function generateSafeSlug($slug) {
+		$slug = preg_replace('/[^a-zA-Z0-9]/', '-', $slug);
+		$slug = strtolower($slug);
+
+		return $slug;
+	}
+
 	/**
          * Go to the previous url.
          */

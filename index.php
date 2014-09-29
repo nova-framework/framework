a9c74020a2758e7b3747c380e01b8e46dbd99ac7
Luca <ldanzelmo@gmail.com>
2014-09-29 23:45:15 +0200

    Separate html from PHP

--- index.php
+++ index.php
@@ -2,9 +2,20 @@
 if(file_exists('vendor/autoload.php')){
 	require 'vendor/autoload.php';
 } else {
-	echo "<h1>Please install via composer.json</h1>";
-	echo "<p>Install Composer instructions: <a href='https://getcomposer.org/doc/00-intro.md#globally'>https://getcomposer.org/doc/00-intro.md#globally</a></p>";
-	echo "<p>Once composer is installed navigate to the working directory in your terminal/command promt and enter 'composer install'</p>";
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+      <title>Simple MVC Framework - Install via Composer</title>
+  </head>
+  <body>
+    <h1>Please install via composer.json</h1>
+      <p>Install Composer instructions: <a href='https://getcomposer.org/doc/00-intro.md#globally'>https://getcomposer.org/doc/00-intro.md#globally</a></p>
+      <p>Once composer is installed navigate to the working directory in your terminal/command promt and enter 'composer install'</p>
+  </body>
+</html>
+<?php
 	exit;
 }
 

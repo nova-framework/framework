6cc5a3058eb7b3a4f3b2e175b53da08273c890fd
David Carr <dave@daveismyname.com>
2014-09-29 22:59:07 +0100

    Added $view for legacy calls

--- app/core/controller.php
+++ app/core/controller.php
@@ -1,7 +1,8 @@
 <?php namespace core;
-use core\config   as Config,
-	core\language as Language,
-    core\error    as Error;
+use core\config as Config,
+    core\view as View,
+    core\language as Language,
+    core\error as Error;
 
 /*
  * controller - base controller
@@ -11,11 +12,21 @@ use core\config   as Config,
  * @date June 27, 2014
  */
 class Controller {
+	
+	/**
+	 * view variable to use the view class
+	 * @var string
+	 */
+	public $view;
 
 	/**
 	 * on run make an instance of the config class and view class
 	 */
 	public function __construct(){
+		
+		//initialise the views object
+		$this->view = new view();
+		
 		//initialise the language object
 		$this->language = new Language();
 	}

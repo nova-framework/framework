cf4e0d98a7748caae546e21fcbac5176212df3a9
QsmaPL <QsmaPL@users.noreply.github.com>
2014-10-05 23:44:55 +0200

    Update model.php

--- app/core/model.php
+++ app/core/model.php
@@ -13,11 +13,12 @@ class Model extends Controller {
 	 * @var object
 	 */
 	protected $_db;
-	
+
 	/**
 	 * create a new instance of the database helper
 	 */
 	public function __construct(){
+
 		//connect to PDO here.
 		$this->_db = new \helpers\database();
 

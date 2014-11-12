e916c4522c6de92beb97ebf8a4bc7c1b2d407c7d
David Carr <dave@daveismyname.com>
2014-11-12 13:55:01 +0000

    updated footer

--- app/templates/default/footer.php
+++ app/templates/default/footer.php
@@ -1,15 +1,25 @@
 </div>
 
-<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-<!-- uncomment to use a local hosted version -->
-<!--<script src="<?php echo \helpers\url::get_template_path();?>js/jquery.js"></script>-->
-<?php echo $data['js']."\n";?>
+<!--
+    You can use jquery from JS dir or get latest script from jQuery servers
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+-->
 
-<script>
-$(document).ready(function(){
-	<?php echo $data['jq']."\n";?>
-});
-</script>
+<script src="<?php echo \helpers\url::get_template_path();?>js/jquery.js"></script>
+
+<!-- JS plugins -->
+<?php if(isset($data['js'])) : ?>
+    <?php echo $data['js']."\n";?>
+<?php endif; ?>
+
+<!-- JS script -->
+<?php if(isset($data['jq'])) : ?>
+    <script>
+        $(document).ready(function(){
+            <?php echo $data['jq']."\n";?>
+        });
+    </script>
+<?php endif; ?>
 
 </body>
 </html>

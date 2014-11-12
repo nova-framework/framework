43f1b950137e8135290290b8e43635629b4443e5
David Carr <dave@daveismyname.com>
2014-11-12 13:52:58 +0000

    added local jQuery

--- app/templates/default/footer.php
+++ app/templates/default/footer.php
@@ -1,6 +1,8 @@
 </div>
 
 <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+<!-- uncomment to use a local hosted version -->
+<!--<script src="<?php echo \helpers\url::get_template_path();?>js/jquery.js"></script>-->
 <?php echo $data['js']."\n";?>
 
 <script>

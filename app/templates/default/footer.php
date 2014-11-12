3df48d277b0907a4bd11de9261aa2a46e429f1c2
QsmaPL <QsmaPL@users.noreply.github.com>
2014-11-12 15:12:03 +0100

    Small footer edit
    I've added foreach for js, this eliminate unnecessary retyping:
    
    	<script src="/js/plugin/my_blabla_plugin.js"></script>
    
    in controller we can use this:
    
    	$data['js'] =  array(
    		'http://mysite.com/a.js',
    		'http://mysite.com/b.js',
    		'http://mysite.com/c.js',
    		'http://mysite.com/d.js',
    	);
    
    or 
    
    	$data['js'] =  array(
    		DIR . 'js/main.js',
    		\helpers\url::get_template_path() . 'js/test.js',
    	);

--- app/templates/default/footer.php
+++ app/templates/default/footer.php
@@ -1,25 +1,31 @@
 </div>
 
 <!--
-    You can use jquery from JS dir or get latest script from jQuery servers
-    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+You can use jquery from JS dir or get latest script from jQuery servers
+<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
 -->
 
 <script src="<?php echo \helpers\url::get_template_path();?>js/jquery.js"></script>
 
-<!-- JS plugins -->
 <?php if(isset($data['js'])) : ?>
-    <?php echo $data['js']."\n";?>
-<?php endif; ?>
 
-<!-- JS script -->
+	<!-- JS plugins -->
+	<?php foreach ($data['js'] as $row): ?>
+		<script src="<?php echo $row; ?>"></script>
+	<?php endforeach ?>
+
+<?php endif ?>
+
 <?php if(isset($data['jq'])) : ?>
-    <script>
-        $(document).ready(function(){
-            <?php echo $data['jq']."\n";?>
-        });
-    </script>
-<?php endif; ?>
+
+	<!-- JS scripts -->
+	<script>
+		$(document).ready(function(){
+			<?php echo $data['jq']."\n";?>
+		});
+	</script>
+
+<?php endif ?>
 
 </body>
 </html>

1ac4c39739bb6325d3b7b0e2fc1acf281438f824
Bartek Kuśmieczuk <QsmaPL@users.noreply.github.com>
2014-11-18 08:12:51 +0100

    Update header.php

--- app/templates/default/header.php
+++ app/templates/default/header.php
@@ -2,7 +2,7 @@
 <html lang="<?php echo LANGUAGE_CODE; ?>">
 <head>
 	<meta charset="utf-8">
-	<title><?php echo $data['title'].' - '.SITETITLE; //SITETITLE defined in index.php?></title>
+	<title><?php echo $data['title'].' - '.SITETITLE; //SITETITLE defined in app/core/config.php ?></title>
 	<link href="<?php echo \helpers\url::get_template_path();?>css/style.css" rel="stylesheet">
 </head>
 <body>

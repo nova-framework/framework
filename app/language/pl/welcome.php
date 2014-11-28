c4d88657a46fd40f277f3ac3d252c28db1a0d0a9
Bartek Kuśmieczuk <QsmaPL@users.noreply.github.com>
2014-11-28 18:23:44 +0100

    Update welcome.php

--- app/language/pl/welcome.php
+++ app/language/pl/welcome.php
@@ -6,14 +6,14 @@ return array(
 	'welcome_text' => 'Witaj',
 	'welcome_message' => '
 		Cześć, witaj z kontrolera welcome! </br>
-		Ta treść może być zmieniona w pliku <code>/app/views/welcome/welcome.php</code>
+		Tą treść możesz zmienić w pliku <code>/app/views/welcome/welcome.php</code>
 	',
 
 	// Subpage method
 	'subpage_text' => 'Podstrona',
 	'subpage_message' => '
 		Cześć, witaj z kontrolera welcome i metody subpage! </br>
-		Ta treść może być zmieniona w pliku <code>/app/views/welcome/subpage.php</code>
+		Tą treść możesz zmienić w pliku <code>/app/views/welcome/subpage.php</code>
 	',
 
 	// Buttons

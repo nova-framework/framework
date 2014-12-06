5a62eb0b72609fc5b890de681cd28fa39ba3c079
David Carr <dave@daveismyname.com>
2014-12-06 00:13:33 +0000

    old style comment
    turn on old style routing makes more sense.

--- index.php
+++ index.php
@@ -70,7 +70,7 @@ Router::any('/subpage', '\controllers\welcome@subpage');
 //if no route found
 Router::error('\core\error@index');
 
-//turn off old style routing
+//turn on old style routing
 Router::$fallback = false;
 
 //execute matched routes

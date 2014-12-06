8d4497c02b20dc329ae4fbc8d8c93a14b36809ec
Volter9 <volter925@gmail.com>
2014-12-05 16:00:51 -0800

    Fixed <br/> tags and added Russian translation

--- app/language/nl/welcome.php
+++ app/language/nl/welcome.php
@@ -5,14 +5,14 @@ return array(
 	// Index method
 	'welcome_text' => 'Welkom',
 	'welcome_message' => '
-		Hallo, welkom van de welcome controller! </br>
+		Hallo, welkom van de welcome controller! <br/>
 		Deze content kan in worden veranderd <code>/app/views/welcome/welcome.php</code>
 	',
 
 	// Subpage method
 	'subpage_text' => 'Subpagina',
 	'subpage_message' => '
-		Hallo, welkom van de welcome controller en methoden subpagina! </br>
+		Hallo, welkom van de welcome controller en methoden subpagina! <br/>
 		Deze content kan in worden veranderd <code>/app/views/welcome/subpage.php</code>
 	',
 

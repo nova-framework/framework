469686285e02253e49ea0d4ff53fdda2d758ae08
Dup Traian <traian.dup@gmail.com>
2015-05-22 07:12:57 +0300

    Update Welcome.php

--- app/language/ro/Welcome.php
+++ app/language/ro/Welcome.php
@@ -3,13 +3,13 @@ return array(
 	// Metoda 'index'
 	'welcome_text' => 'Bun venit',
 	'welcome_message' => '
-		Bună, bine ai venit la controlerul <code>welcome</code>! <br/>
+		Bună, bun venit la controlerul <code>welcome</code>! <br/>
 		Acest conținut poate fi modificat în <code>/app/views/welcome/welcome.php</code>
 	',
 	// Metoda 'subpage'
 	'subpage_text' => 'Subpagina',
 	'subpage_message' => '
-		Bună, bine ai venit la controlerul <code>welcome</code> și metoda <code>subpage</code>! <br/>
+		Bună, bun venit la controlerul <code>welcome</code> și metoda <code>subpage</code>! <br/>
 		Acest conținut poate fi modificat în <code>/app/views/welcome/subpage.php</code>
 	',
 	// Butoane

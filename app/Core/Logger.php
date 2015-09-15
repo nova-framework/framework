53ed5cb568bf0b9aa3d598e3d8bf9ca3a499f2c0
David Carr <dave@daveismyname.com>
2015-09-15 18:47:49 +0100

    Updated paths & cleaned tags

--- app/Core/Logger.php
+++ app/Core/Logger.php
@@ -36,7 +36,7 @@ class Logger
     * path to error file
     * @var boolean
     */
-    public static $errorFile = 'errorlog.html';
+    public static $errorFile = SMVC.'errorlog.html';
 
     /**
     * in the event of an error show this message
--- app/Helpers/Tags.php
+++ app/Helpers/Tags.php
@@ -55,37 +55,6 @@ class Tags
         //site email address
         $string = str_replace('[siteemail]', SITEEMAIL, $string);
 
-        //facebook like box
-        $string = preg_replace_callback("(\[facebooklikebox(.*?)])is", function ($matches) {
-            $params = tags::clean($matches);
-
-            //if key exits use it
-            $username  = (isset($params['username']) ? $params['username'] : '');
-            $width  = (isset($params['width']) ? $params['width'] : '300px');
-            $height  = (isset($params['height']) ? $params['height'] : '258px');
-            $colorscheme  = (isset($params['colorscheme']) ? $params['colorscheme'] : 'light');
-            $showfaces  = (isset($params['showfaces']) ? $params['showfaces'] : 'true');
-            $header  = (isset($params['header']) ? $params['header'] : 'true');
-            $stream  = (isset($params['stream']) ? $params['stream'] : 'false');
-            $showborder  = (isset($params['showborder']) ? $params['showborder'] : 'false');
-            $scrolling  = (isset($params['scrolling']) ? $params['scrolling'] : 'no');
-
-            $likebox = "<iframe src='//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2F$username&amp;
-            width=$width&amp;
-            height=$height&amp;
-            colorscheme=$colorscheme&amp;
-            show_faces=$showfaces&amp;
-            header=$header&amp;
-            stream=$stream&amp;
-            show_border=$showborder&amp;
-            appId=262411407271009'
-            scrolling='$scrolling'
-            frameborder='0'
-            style='border:none; overflow:hidden; width:$width; height:$height;'
-            allowTransparency='true'></iframe>";
-            return $likebox;
-        }, $string);
-
         //feedburner subscribe form
         $string = preg_replace_callback("(\[feedburner(.*?)])is", function ($matches) {
             $params = tags::clean($matches);
@@ -108,25 +77,6 @@ class Tags
 
             return "<script src='https://apis.google.com/js/platform.js' async defer></script>
                     <g:page href='https://plus.google.com/+$username'></g:page>";
-        }, $string);
-
-        //google adbox
-        $string = preg_replace_callback("(\[googleadbox])is", function ($matches) {
-            ob_start();
-            ?>
-            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-            <!-- dcsidebar -->
-            <ins class="adsbygoogle"
-                 style="display:block"
-                 data-ad-client="ca-pub-0401085377924210"
-                 data-ad-slot="3065264368"
-                 data-ad-format="auto"></ins>
-            <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-            </script>
-            <?php
-            $ad = ob_get_clean();
-            return $ad;
         }, $string);
 
         //twitter follow button
@@ -218,62 +168,6 @@ class Tags
             $height  = (isset($params['height']) ? $params['height'] : '360');
 
             return "<iframe width='$width' height='$height' src='https://player.vimeo.com/video/$video' frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>";
-        }, $string);
-
-        //pages get children
-        $string = preg_replace_callback("(\[pages (.*?)])is", function ($matches) {
-            $params = tags::clean($matches);
-            $items = null;
-
-            //if key exits use it
-            $parent  = (isset($params['parent']) ? $params['parent'] : '');
-            $class   = (isset($params['class']) ? $params['class'] : '');
-            $id      = (isset($params['id']) ? $params['id'] : '');
-            $subclass = (isset($params['subclass']) ? $params['subclass'] : '');
-            $subid    = (isset($params['subid']) ? $params['subid'] : '');
-
-            $db = Database::get();
-
-            $q = $db->select("SELECT pageID,pageParent,pageMenuTitle,pageSlug FROM ".PREFIX."pages WHERE pageParent=:parent AND pageStandAlone='0' ORDER BY pageOrder", array(':parent' => $parent));
-
-            //if page is more then 0 then show drop down menu
-            if (count($q) > 0) {
-                $items = "<ul class='$class' id='$id'>\n";
-                foreach ($q as $row) {
-                    //if slug is external use it
-                    if (preg_match("/http/i", $row->pageSlug)) {
-                        $rowSlug = $row->pageSlug;
-                    } else {
-                        //otherwise add in the DIR
-                        $rowSlug = DIR.$row->pageSlug;
-                    }
-
-                    $items.="<li><a href='$rowSlug'>$row->pageMenuTitle</a>";
-
-                    $q2 = $db->select("SELECT pageMenuTitle,pageSlug FROM ".PREFIX."pages WHERE pageParent=:parent AND pageStandAlone='0' ORDER BY pageOrder", array(':parent' => $row->pageID));
-                    if (count($q2) > 0) {
-                        $items.= "<ul class='$subclass' id='$subid'>\n";
-                        foreach ($q2 as $row2) {
-                            //if slug is external use it
-                            if (preg_match("/http/i", $row2->pageSlug)) {
-                                $row2Slug = $row2->pageSlug;
-                            } else {
-                                //otherwise add in the DIR
-                                $row2Slug = DIR.$row2->pageSlug;
-                            }
-
-                            $items.="<li><a href='$row2Slug'>$row2->pageMenuTitle</a></li>";
-                        }
-                        $items.="</ul>";
-                    }
-                    $items.="</li>";
-
-                }
-                $items.="</ul>";
-            }
-
-            return $items;
-
         }, $string);
 
         return $string;

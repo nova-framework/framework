05e672d3905960aea8f048bfc0dd538ce43ba83f
gabizz <gmaftei@gmail.com>
2015-11-02 22:52:17 +0200

    added PORT for PDO connection to database
    useful when running locally several instances of MySQL. also when
    deploying a desktop app with phpDesktop and the customer already uses
    MySQL on 3306 for other purposes.

--- app/Helpers/Database.php
+++ app/Helpers/Database.php
@@ -32,22 +32,24 @@ class Database extends PDO
     {
         // Determining if exists or it's not empty, then use default group defined in config
         $group = !$group ? array (
-            'type' => DB_TYPE,
-            'host' => DB_HOST,
-            'name' => DB_NAME,
-            'user' => DB_USER,
-            'pass' => DB_PASS
+			'type' => DB_TYPE,
+			'host' => DB_HOST,
+			'port' => DB_PORT,			
+			'name' => DB_NAME,
+			'user' => DB_USER,
+			'pass' => DB_PASS
         ) : $group;
 
         // Group information
-        $type = $group['type'];
-        $host = $group['host'];
-        $name = $group['name'];
-        $user = $group['user'];
-        $pass = $group['pass'];
+		$type = $group['type'];
+		$host = $group['host'];
+		$port = $group['port'];
+		$name = $group['name'];
+		$user = $group['user'];
+		$pass = $group['pass'];
 
         // ID for database based on the group information
-        $id = "$type.$host.$name.$user.$pass";
+        $id = "$type.$host.$port.$name.$user.$pass";
 
         // Checking if the same
         if (isset(self::$instances[$id])) {
@@ -58,7 +60,7 @@ class Database extends PDO
             // I've run into problem where
             // SET NAMES "UTF8" not working on some hostings.
             // Specifiying charset in DSN fixes the charset problem perfectly!
-            $instance = new Database("$type:host=$host;dbname=$name;charset=utf8", $user, $pass);
+            $instance = new Database("$type:host=$host;port=$port;dbname=$name;charset=utf8", $user, $pass);
             $instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             // Setting Database into $instances to avoid duplication

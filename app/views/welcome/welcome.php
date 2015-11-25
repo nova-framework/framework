a13cf545a3c13dfded18deeeb02c12ebfaba31cf
Bryan Y <cs337bryan@gmail.com>
2015-11-24 23:47:47 -0800

    added some samples

--- app/views/welcome/welcome.php
+++ app/views/welcome/welcome.php
@@ -3,7 +3,8 @@
  * Sample layout
  */
 
-use Core\Language;
+use Core\Language,
+	Helpers\Form;
 
 ?>
 
@@ -16,3 +17,31 @@ use Core\Language;
 <a class="btn btn-md btn-success" href="<?php echo DIR;?>subpage">
 	<?php echo Language::show('open_subpage', 'Welcome'); ?>
 </a>
+<hr>
+<?php
+echo Form::openPOST(["role"=>"form"]);
+echo '<br><label>Button Button</label><br>';
+echo Form::buttonButton(["value"=>"test",
+					     "class"=>["span","glyphicon glyphicon-off"]
+						],
+						["class"=>"btn btn-md btn-success",
+						 "disabled"]);
+
+echo '<br><br><label>Input Button</label><br>';
+echo Form::buttonInput(["class"=>"btn btn-md btn-danger",
+						"value"=>"Button Input"]);
+
+echo '<br><br><label>Text Input</label><br>';
+echo Form::textInput(["class"=>"form-control",
+		 			  "placeholder"=>"placeholder text"]);
+
+echo '<br><br><label>Password Input</label><br>';
+echo Form::passwordInput(["class"=>"form-control",
+						  "placeholder"=>"placeholder password"]);
+
+echo '<br><br><label>Checkboxes</label><br>';
+echo Form::checkbox(["Check 1" => [["id"=>"check1"],["class"=>"haha"]],
+					 "Second" => [["class"=>"tiger"],["checked ","class"=>"form-control"]]]);
+echo Form::close();
+
+?>

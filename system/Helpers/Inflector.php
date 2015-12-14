18e73517b318f8c98151bd9a8ea9bfd77e34085e
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2015-12-14 12:46:14 +0100

    Small changes on \Helpers\Inflector

--- system/Helpers/Inflector.php
+++ system/Helpers/Inflector.php
@@ -1,6 +1,15 @@
-<?php namespace Helpers;
+<?php
+/**
+ * Inflector Helper
+ *
+ * @author Virgil-Adrian Teaca - virgil@giulianaeassociati.com
+ * @version 1.0
+ * @date Dec 14, 2015
+ */
 
+namespace Helpers;
 
+ 
 class Inflector extends \Doctrine\Common\Inflector\Inflector
 {
 }

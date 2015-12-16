7c1231714fdb01cc4c35d3f441a6579431d82037
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2015-12-16 21:42:00 +0100

    Using the DIRECTORY_SEPARATOR even in the server.php file.

--- server.php
+++ server.php
@@ -1,8 +1,13 @@
 <?php
 
-$public_path = __DIR__.'/public/';
+// This file allows us to emulate Apache's "mod_rewrite" functionality from the built-in PHP web server.
+// Provides a convenient way to test the MVC application without having installed a "real" web server software.
+
+define('DS', DIRECTORY_SEPARATOR);
 
 //
+$public_path = dirname(__DIR__).DS.'public'.DS;
+
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 $uri = urldecode($uri);
@@ -10,8 +15,6 @@ $uri = urldecode($uri);
 //
 $requested = $public_path .$uri;
 
-// This file allows us to emulate Apache's "mod_rewrite" functionality from the built-in PHP web server.
-// Provides a convenient way to test the MVC application without having installed a "real" web server software.
 if (($uri !== '/') && file_exists($requested)) {
     return false;
 }

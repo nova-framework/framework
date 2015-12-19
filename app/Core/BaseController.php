6c0cecd1dbe0f4ff262fb73630294ffc0af108e2
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2015-12-19 02:23:46 +0100

    Adjust a comment in \App\Core\BaseController

--- app/Core/BaseController.php
+++ app/Core/BaseController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * ClassicController - Base Class for all Classic Controllers.
+ * ClassicController - Base Class for all App Controllers.
  *
  * @author Virgil-Adrian Teaca - virgil@@giulianaeassociati.com
  * @version 3.0

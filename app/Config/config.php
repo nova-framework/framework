4f48c6c5119738a207ee35fdddeba5a15bb7b130
Tom Valk <tomvalk@gmail.com>
2015-12-19 13:35:55 +0100

    Adding intents to database config. for better readability.

--- app/Config/config.php
+++ app/Config/config.php
@@ -60,13 +60,13 @@ Config::set('database', array(
     'default' => array(
         'engine' => 'mysql',
         'config' => array(
-            'host' => 'localhost',
-            'port' => 3306, // Not required, default is 3306
-            'database' => 'dbname',
-            'user' => 'root',
-            'password' => 'password',
-            'fetch_method' => \PDO::FETCH_OBJ, // Not required, default is OBJ.
-            'charset' => 'utf8' // Not required, default and recommended is utf8.
+            'host'          => 'localhost',
+            'port'          => 3306, // Not required, default is 3306
+            'database'      => 'dbname',
+            'user'          => 'root',
+            'password'      => 'password',
+            'fetch_method'  => \PDO::FETCH_OBJ, // Not required, default is OBJ.
+            'charset'       => 'utf8' // Not required, default and recommended is utf8.
         )
     ),
     /** Extra connections can be added here, some examples: */

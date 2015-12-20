cd08c9cbfc0bc01b8f77f385a0ab0e22facac3f6
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2015-12-20 17:26:34 +0100

    Simplify the Configuration values for \Nova\Helpers\PhpMailer

--- app/Config/config.php
+++ app/Config/config.php
@@ -35,19 +35,19 @@ Config::set('languages', array(
  * Email (PHPMailer) configuration
  */
 Config::set('emailer', array(
-    'mailer_charset'       => 'iso-8859-1',
-    'mailer_from_name'     => 'SMVC Website',
-    'mailer_from_email'    => 'smvc@localhost',
-    'mailer_mailer'        => 'mail',           // Could be 'mail' => 'sendmail' or 'smtp'
+    'charset'       => 'iso-8859-1',
+    'from_name'     => 'SMVC Website',
+    'from_email'    => 'smvc@localhost',
+    'mailer'        => 'mail',           // Could be 'mail' => 'sendmail' or 'smtp'
 
     /** Only when using smtp as mailer: */
-    'mailer_smtp_host'     => 'localhost',
-    'mailer_smtp_port'     => 25,
-    'mailer_smtp_secure'   => '',    // Options: '' => 'ssl' or 'tls'
-    'mailer_smtp_auth'     => false, // Use SMTPAuth, (false or true)
-    'mailer_smtp_user'     => '',    // Only when using SMTPAuth
-    'mailer_smtp_pass'     => '',    // Only when using SMTPAuth
-    'mailer_smtp_authtype' => ''     // Options are LOGIN (default), PLAIN, NTLM, CRAM-MD5. Blank when not use SMTPAuth.
+    'smtp_host'     => 'localhost',
+    'smtp_port'     => 25,
+    'smtp_secure'   => '',    // Options: '' => 'ssl' or 'tls'
+    'smtp_auth'     => false, // Use SMTPAuth, (false or true)
+    'smtp_user'     => '',    // Only when using SMTPAuth
+    'smtp_pass'     => '',    // Only when using SMTPAuth
+    'smtp_authtype' => ''     // Options are LOGIN (default), PLAIN, NTLM, CRAM-MD5. Blank when not use SMTPAuth.
 ));
 
 

768a451460a3cdbeca754e5cc270ed9e0ec170c6
Martin Hering <nekomajin@gmx.de>
2015-12-22 10:19:39 +0100

    3.0 fix typos

--- public/index.php
+++ public/index.php
@@ -21,7 +21,7 @@ if (file_exists(BASEPATH.'vendor'.DS.'autoload.php')) {
 } else {
     echo "<h1>Please install via composer.json</h1>";
     echo "<p>Install Composer instructions: <a href='https://getcomposer.org/doc/00-intro.md#globally'>https://getcomposer.org/doc/00-intro.md#globally</a></p>";
-    echo "<p>Once composer is installed navigate to the working directory in your terminal/command promt and enter 'composer install'</p>";
+    echo "<p>Once composer is installed navigate to the working directory in your terminal/command prompt and enter 'composer install'</p>";
     exit;
 }
 

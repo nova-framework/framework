f475cec503ba53a3c0f6cf59b6eb7d6543101ce5
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2015-12-23 19:03:14 +0100

    Adjust logic in \Nova\Helpers\PhpMailer

--- system/Helpers/PhpMailer.php
+++ system/Helpers/PhpMailer.php
@@ -25,23 +25,27 @@ class PhpMailer extends \PHPMailer
     {
         parent::__construct();
 
+        // Get the Mailer configuration.
         $config = Config::get('emailer');
 
-        // Set all config in myself
-        $this->CharSet = $config['charset'];
+        // Set all Config options to phpMailer engine.
+        $this->CharSet  = $config['charset'];
         $this->FromName = $config['from_name'];
-        $this->From = $config['from_email'];
-        $this->Mailer = $config['mailer'];
+        $this->From     = $config['from_email'];
+        $this->Mailer   = $config['mailer'];
 
-        if ($this->Mailer === 'smtp')
-        {
-            $this->Host = $config['smtp_host'];
-            $this->Port = $config['smtp_port'];
-            $this->SMTPSecure = $config['smtp_secure'];
-            $this->SMTPAuth = $config['smtp_auth'];
-            $this->Username = $config['smtp_user'];
-            $this->Password = $config['smtp_pass'];
-            $this->AuthType = $config['smtp_authtype'];
+        if ($this->Mailer !== 'smtp') {
+            // Let's make Tom happy!
+            return null;
         }
+
+        // SMTP only options.
+        $this->Host       = $config['smtp_host'];
+        $this->Port       = $config['smtp_port'];
+        $this->SMTPSecure = $config['smtp_secure'];
+        $this->SMTPAuth   = $config['smtp_auth'];
+        $this->Username   = $config['smtp_user'];
+        $this->Password   = $config['smtp_pass'];
+        $this->AuthType   = $config['smtp_authtype'];
     }
 }

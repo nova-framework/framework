3ed2f205d27642f2642c7529f77c4e9ab363e527
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2015-12-23 18:55:30 +0100

    Introduce the alternative \Nova\Net\Response::redirect method as encouraged redirect function for new projects

--- system/Net/Response.php
+++ system/Net/Response.php
@@ -65,6 +65,25 @@ class Response
         505 => 'HTTP Version Not Supported'
     );
 
+
+    /**
+     * Redirect to chosen URL.
+     *
+     * @param  string  $url      the URL to redirect to
+     * @param  boolean $fullpath if true use only url in redirect instead of using site_url()
+     */
+    public static function redirect($url = null, $fullPath = false)
+    {
+        if ($fullPath !== false) {
+            $url = site_url($url);
+        }
+
+        header('Location: '.$url);
+
+        // Stop processing and quit.
+        exit;
+    }
+
     /**
      * Sets a status code by taking the number and automatically adding the status text
      *

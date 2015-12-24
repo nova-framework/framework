035ff53dd119c7899ce33986a2f2a3fc5f0fda64
Tom Valk <tomvalk@gmail.com>
2015-12-24 09:21:58 +0100

    Make use of one abstract database service class.

--- system/Database/Service.php
+++ system/Database/Service.php
@@ -32,31 +32,71 @@ abstract class Service
 
     /**
      * DatabaseService constructor.
-     * @param Engine|null $engine
+     * @param Engine|string|null $engine Engine instance or connection name from config.
      */
-    public function __construct($engine = null)
+    public function __construct($engine = 'default')
     {
-        if ($engine === null || !$engine instanceof Engine)
+        if (! $engine instanceof Engine)
         {
-            $engine = Manager::getEngine();
+            $engine = Manager::getEngine($engine);
         }
 
         $this->engine = $engine;
     }
 
+    /**
+     * Set engine for this service.
+     * @param Engine $engine
+     */
+    public function setEngine($engine)
+    {
+        if (!$engine instanceof Engine) {
+            throw new \UnexpectedValueException("Engine should be an instance of one of the Engines!");
+        }
+        $this->engine = $engine;
+    }
 
     /**
-     * Create the entity in the database. Will try to insert it into the database
+     * Create the entity (or entities) in the database. Will try to insert it into the database
      * Can throw Exceptions on failure or return false.
      *
      * On success it will return the entity including the (optional) inserted ID (primary key, when only one)
      *
-     * @param $entity Entity|Entity[] One ore multiple entit(y|ies), only when engine and service supports multiple entities!
+     * @param $entity Entity|Entity[] One or multiple entit(y|ies) to create in the database
      * @return false|Entity
      * @throws \Exception
      */
-    abstract public function create($entity);
+    public function create($entity)
+    {
+        // If it isn't already an array, make it an array, to keep code simple.
+        if (!is_array($entity)) {
+            $entity = array($entity);
+        }
 
+        // Loop and insert
+        foreach($entity as $idx => $entit)
+        {
+            // Insert
+            $result = $this->engine->insert(DB_PREFIX . $this->table, get_object_vars($entit));
+            if ($result === false) {
+                // On error, return inmidiate.
+                return false;
+            }
+
+            // If only one Primary Key, we will set it in the entity.
+            if (count($this->primaryKeys) == 1 && $entit->{$this->primaryKeys[0]} == null) {
+                $entity[$idx]->{$this->primaryKeys[0]} = $result;
+            }
+
+        }
+
+        // Return same format as before
+        if (count($entity) === 1) {
+            return $entity[0];
+        }
+
+        return $entity;
+    }
 
     /**
      * Read entities with the $sql query, must always give a full query, including the prefix and where's.
@@ -67,11 +107,13 @@ abstract class Service
      *
      * @param $sql string
      * @param $bind array
-     * @return false|Entity[]
+     * @return false|Entity[]|object
      * @throws \Exception
      */
-    abstract public function read($sql, $bind = array());
-
+    public function read($sql, $bind = array())
+    {
+        return $this->engine->select($sql, $bind, $this->fetchMethod, $this->fetchClass);
+    }
 
     /**
      * Will update the entity in the database. You could also give an array with entities. We will automaticly detect
@@ -80,23 +122,50 @@ abstract class Service
      * Make sure you don't change your primary keys! As this will be used to execute the update with
      * For safety it will default limit on 1 row only, you can override it but be warned on this!
      *
-     * @param $entity Entity|Entity[]
-     * @param $limit int Limit of changes, may not be effective on every driver! Default 1
+     * @param $entity Entity
+     * @param $limit int Limit of changes, may not be effective on every driver! Default 1.
      * @return false|Entity
      * @throws \Exception
      */
-    abstract public function update($entity, $limit = 1);
+    public function update($entity, $limit = 1)
+    {
+        $primaryValues = array();
 
+        foreach($this->primaryKeys as $pk) {
+            $primaryValues[$pk] = $entity->{$pk};
+        }
+
+        $result = $this->engine->update(DB_PREFIX . $this->table, get_object_vars($entity), $primaryValues, $limit);
+        if ($result === false) {
+            return false;
+        }
+
+        // Primary Key, put it back into the entity.
+        if (count($this->primaryKeys) == 1 && $entity->{$this->primaryKeys[0]} == null) {
+            $entity->{$this->primaryKeys[0]} = $result;
+        }
+
+        return $entity;
+    }
 
     /**
      * Delete an entity from the database. Can also handle multiple entities with an array given in the $entity parameter
      *
-     * For safety it will default limit on 1 row only, you can override it but be warned on this!
+     * For safety it will limit on 1 row only by default, you can disable by giving null into the limit.
      *
-     * @param $entity Entity|Entity[]
-     * @param $limit int Limit of changes, may not be effective on every driver! Default 1
+     * @param $entity Entity
+     * @param $limit int|null Limit of changes, may not be effective on every driver! Default 1. Null for infinity.
      * @return boolean successful delete?
      * @throws \Exception
      */
-    abstract public function delete($entity, $limit = 1);
+    public function delete($entity, $limit = 1)
+    {
+        $primaryValues = array();
+
+        foreach($this->primaryKeys as $pk) {
+            $primaryValues[$pk] = $entity->{$pk};
+        }
+
+        return $this->engine->delete(DB_PREFIX . $this->table, $primaryValues, $limit) !== false;
+    }
 }

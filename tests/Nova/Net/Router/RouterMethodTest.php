c23b25a4fbf3f65fa5fdb0c710abb3c449a0fe64
Tom Valk <tomvalk@gmail.com>
2015-12-25 12:08:23 +0100

    Adding old style route declaration test in basic get test.

--- tests/Nova/Net/Router/RouterMethodTest.php
+++ tests/Nova/Net/Router/RouterMethodTest.php
@@ -20,29 +20,43 @@ class RouterMethodTest extends \PHPUnit_Framework_TestCase
 
     /**
      * @covers \Nova\Net\Router::addRoute
-     * @covers \Nova\Net\Router::invokeController
+     * @covers \Nova\Net\Router::__callStatic
      * @covers \Nova\Net\Router::dispatch
      */
     public function testBasicGet()
     {
-        $expected = true;
-        $current = false;
+        $expected_1 = true;
+        $expected_2 = true;
+        $current_1 = false;
+        $current_2 = false;
 
-        // Add route
-        \Nova\Net\Router::getInstance()->addRoute('get', '/test/get/1', function() use(&$current) {
-            $current = true;
+        // Add route with the new method
+        \Nova\Net\Router::getInstance()->addRoute('get', '/test/get/basic/1', function() use(&$current_1) {
+            $current_1 = true;
         });
 
-        // Test running the route
-        $this->spoofRouter('/test/get/1');
+        // Add route with old method
+        \Nova\Net\Router::get('/test/get/basic/2', function() use(&$current_2) {
+            $current_2 = true;
+        });
 
-        // Dispatch the router
+        // Test running the route, first one
+        $this->spoofRouter('/test/get/basic/1');
+        \Nova\Net\Router::getInstance()->dispatch();
+
+        // Second route
+        $this->spoofRouter('/test/get/basic/2');
         \Nova\Net\Router::getInstance()->dispatch();
 
         // Run detecter
-        $this->assertEquals($expected, $current, 'Router should call the callback!');
+        $this->assertEquals($expected_1, $current_1, 'Router should call the callback!');
+        $this->assertEquals($expected_2, $current_2, 'Router should call the callback!');
     }
 
+    /**
+     * @covers \Nova\Net\Router::addRoute
+     * @covers \Nova\Net\Router::dispatch
+     */
     public function testParameterGet()
     {
 

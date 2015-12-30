79aadb010c0df05ba82ecaceaa398cef1f10b669
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2015-12-30 03:07:34 +0100

    Small change in App\Modules\Demo\Controllers\Models

--- app/Modules/Demo/Controllers/Models.php
+++ app/Modules/Demo/Controllers/Models.php
@@ -58,7 +58,7 @@ class Models extends BaseController
         //
         $members = $this->model->limit(2, 0)->findAll();
 
-        $message .= '<h4>$this->model->limit(2, 0)->findAll();</h4><pre>'.var_export($members, true).'</pre>';
+        $message .= '<h4>$this->model->limit(2, 0)->findAll();</h4><pre>'.var_export($members, true).'</pre><br>';
 
         //
         $userInfo = array(
@@ -67,46 +67,46 @@ class Models extends BaseController
             'email'    => 'virgil@novaframwork.dev'
         );
 
-        $message .= '<h4>$userInfo</h4><pre>'.var_export($userInfo, true).'</pre>';
+        $message .= '<h4>$userInfo</h4><pre>'.var_export($userInfo, true).'</pre><br>';
 
         $retval = $this->model->insert($userInfo);
 
-        $message .= '<h4>$this->model->insert($userInfo);</h4><pre>'.var_export($retval, true).'</pre>';
+        $message .= '<h4>$this->model->insert($userInfo);</h4><pre>'.var_export($retval, true).'</pre><br>';
 
         //
         $members2 = $this->model->findAll();
 
-        $message .= '<h4>$this->model->findAll()</h4><pre>'.var_export($members2, true).'</pre>';
+        $message .= '<h4>$this->model->findAll()</h4><pre>'.var_export($members2, true).'</pre><br>';
 
         //
         $retval = $this->model->deleteBy('username', 'virgil');
 
-        $message .= '<h4>$this->model->deleteBy(\'username\', \'virgil\');</h4><pre>'.var_export($retval, true).'</pre>';
+        $message .= '<h4>$this->model->deleteBy(\'username\', \'virgil\');</h4><pre>'.var_export($retval, true).'</pre><br>';
 
         //
         $members3 = $this->model->findAll();
 
-        $message .= '<h4>$this->model->findAll()</h4><pre>'.var_export($members3, true).'</pre>';
+        $message .= '<h4>$this->model->findAll()</h4><pre>'.var_export($members3, true).'</pre><br>';
 
         //
         $members4 = $this->model->orderBy('username', 'desc')->limit(2, 0)->findAll();
 
-        $message .= '<h4>$this->model->orderBy(\'username\', \'desc\')->limit(2, 0)->findAll();</h4><pre>'.var_export($members4, true).'</pre>';
+        $message .= '<h4>$this->model->orderBy(\'username\', \'desc\')->limit(2, 0)->findAll();</h4><pre>'.var_export($members4, true).'</pre><br>';
 
         //
         $result = $this->model->findBy('username', 'marcus');
 
-        $message .= '<h4>$this->model->findBy(\'username\', \'marcus\');</h4><pre>'.var_export($result, true).'</pre>';
+        $message .= '<h4>$this->model->findBy(\'username\', \'marcus\');</h4><pre>'.var_export($result, true).'</pre><br>';
 
         //
-        $result = $this->model->find(1);
+        $result = $this->model->find(3);
 
-        $message .= '<h4>$this->model->find(1)</h4><pre>'.var_export($result, true).'</pre>';
+        $message .= '<h4>$this->model->find(3);</h4><pre>'.var_export($result, true).'</pre><br>';
 
         //
         $members5 = $this->model->orderBy('username', 'desc')->findMany(array(1, 3));
 
-        $message .= '<h4>$this->model->orderBy(\'username\', \'desc\')->findMany(array(1, 3));</h4><pre>'.var_export($members5, true).'</pre>';
+        $message .= '<h4>$this->model->orderBy(\'username\', \'desc\')->findMany(array(1, 3));</h4><pre>'.var_export($members5, true).'</pre><br>';
 
         // Setup the View variables.
         $this->title(__d('demo', 'BaseModel Demo'));

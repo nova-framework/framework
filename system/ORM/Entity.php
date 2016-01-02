130956722c2fca82f9c6754385fa4d646532e899
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-01-02 13:09:42 +0100

    Initial implementation of \Nova\ORM

---
+++ system/ORM/Entity.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Abstract Entity.
+ *
+ * @author Tom Valk - tomvalk@lt-box.info
+ * @version 3.0
+ * @date December 19th, 2015
+ */
+
+namespace Nova\ORM;
+
+/**
+ * Class Entity, can be extended with your database entities
+ * @package Core\Database
+ */
+abstract class Entity
+{
+
+}

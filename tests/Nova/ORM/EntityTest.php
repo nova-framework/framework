2fa2d6e74f0e655b345fcace17667495ccafb9e2
Tom Valk <tomvalk@gmail.com>
2016-01-04 14:47:59 +0100

    Adding test for columns in entity.

--- tests/Nova/ORM/EntityTest.php
+++ tests/Nova/ORM/EntityTest.php
@@ -10,5 +10,9 @@ class EntityTest extends \PHPUnit_Framework_TestCase
     public function testBasic()
     {
         $car = new Car();
+
+        $cols = Structure::getTableColumns($car);
+
+        $this->assertEquals(4, count($cols));
     }
 }

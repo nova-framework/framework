af3a226806d410a3d56e87c04f4a0d74168a0efe
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-01-04 22:55:22 +0100

    Add the method \Nova\Input\Filter::addRules to permit to specify the Input Validation and Filtration Rules via an array.

--- system/Input/Filter.php
+++ system/Input/Filter.php
@@ -10,8 +10,36 @@
 namespace Nova\Input;
 
 use Sirius\Input\InputFilter;
+use Sirius\Validation\ValidatorInterface;
+use Sirius\Filtration\FiltratorInterface;
+
+use Nova\Input\Element\Factory as ElementFactory;
 
 
 class Filter extends InputFilter
 {
+
+    public function __construct(ElementFactory $elementFactory = null, ValidatorInterface $validator = null, FiltratorInterface $filtrator = null)
+    {
+        parent::__construct($elementFactory, $validator, $filtrator);
+    }
+
+    public function setRules($rules)
+    {
+        // Check for valid data.
+        if (! is_array($rules)) {
+            throw new \Exception("Rules parameter must be an Array.");
+        }
+
+        // Firstly, clear all Elements.
+        $this->elements = array();
+
+        $this->elementsIndex = PHP_INT_MAX;
+
+        // Add the given Elements, one by one.
+        foreach($rules as $fieldName => $fieldRules) {
+            $this->addElement($fieldName, $fieldRules);
+        }
+    }
+
 }

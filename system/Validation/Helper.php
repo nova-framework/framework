ad785128a414f18d1aff25c7147ca53b60c8022e
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-01-04 16:48:08 +0100

    Fix into \Nova\DBAL\Connection::fetchClass and initial implementation of Sirius based Validation

---
+++ system/Validation/Helper.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Nova\Validation;
+
+use Sirius\Validation\Helper as BaseHelper;
+
+
+class Helper extends BaseHelper
+{
+}
---
+++ system/Validation/Rule/Callback.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Nova\Validation\Rule;
+
+use Sirius\Validation\Rule\Callback as BaseCallback;
+
+
+class Callback extends BaseCallback
+{
+}

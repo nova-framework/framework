a808326c0d2838c28c9e03a73dc8e87b723a23fd
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-01-05 00:38:58 +0100

    Complete the \Nova\Input Filtration and Validation

--- system/Input/Validation/Validator.php
+++ system/Input/Validation/Validator.php
@@ -9,9 +9,24 @@
 
 namespace Nova\Input\Validation;
 
+use Sirius\Validation\ErrorMessage;
 use Sirius\Validation\Validator as BaseValidator;
+
+use Nova\Input\Validation\RuleFactory;
 
 
 class Validator extends BaseValidator
 {
+    public function __construct(RuleFactory $ruleFactory = null, ErrorMessage $errorMessagePrototype = null)
+    {
+        if ($ruleFactory === null) {
+            $ruleFactory = new RuleFactory();
+        }
+
+        if ($errorMessagePrototype === null) {
+            $errorMessagePrototype = new ErrorMessage();
+        }
+
+        parent::__construct($ruleFactory, $errorMessagePrototype);
+    }
 }

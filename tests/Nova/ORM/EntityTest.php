0902a84588fdffb122e725d517f9f6498a3ac66c
Tom Valk <tomvalk@gmail.com>
2016-01-05 14:24:43 +0100

    Adding multiple find tests.

--- tests/Nova/ORM/EntityTest.php
+++ tests/Nova/ORM/EntityTest.php
@@ -40,6 +40,47 @@ class EntityTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('\App\Modules\Demo\Models\Entities\Car', $car);
     }
 
+    /**
+     * @covers \Nova\ORM\Entity
+     * @covers \Nova\ORM\Entity::save
+     * @covers \Nova\ORM\Entity::find
+     * @throws \Exception
+     */
+    public function testBasicFinding()
+    {
+        $car = new Car();
+
+        // Insert 1
+        $car->make = 'Nova Cars';
+        $car->model = 'Framework_ORM_Test_Search_1';
+        $car->costs = 50000;
+
+        $insert = $car->save();
+        $this->assertEquals(1, $insert);
+
+        $car2 = new Car();
+
+        // Insert 2
+        $car2->make = 'Nova Cars';
+        $car2->model = 'Framework_ORM_Test_Search_2';
+        $car2->costs = 50000;
+
+        $insert = $car2->save();
+        $this->assertEquals(1, $insert);
+
+
+
+        // Search single
+        $searched = Car::find($car->carid);
+        $this->assertInstanceOf('\App\Modules\Demo\Models\Entities\Car', $searched);
+
+        $searched = Car::find(array('model' => 'Framework_ORM_Test_Search_1'));
+        $this->assertInstanceOf('\App\Modules\Demo\Models\Entities\Car', $searched);
+
+        $searched = Car::find(array('model' => 'Framework_ORM_Test_Search_1', 'costs' => 50000));
+        $this->assertInstanceOf('\App\Modules\Demo\Models\Entities\Car', $searched);
+    }
+
 
     /**
      * @covers \Nova\ORM\Entity

14bb8c3f276093e3da86e6782432e43c1e779b93
Tom Valk <tomvalk@gmail.com>
2016-01-05 15:55:48 +0100

    Using correct directory seps in bootstrap (test bootstrap) for sqlite db path.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -40,6 +40,7 @@ require_once $configDir.'config.example.php';
 Config::set('database', array(
     'default' => array(
         'engine' => 'mysql',
+        'driver' => 'pdo_mysql',
         'config' => array(
             'host'        => 'localhost',
             'port'        => 3306,     // Not required, default is 3306
@@ -54,7 +55,7 @@ Config::set('database', array(
         'engine' => 'sqlite',
         'driver'  => 'pdo_sqlite',
         'config' => array(
-            'path'        => BASEPATH .'storage/persistent/test.sqlite',
+            'path'        => BASEPATH .'storage'.DS.'persistent'.DS.'test.sqlite',
             'return_type' => 'object' // Not required, default is 'array'.
         )
     )

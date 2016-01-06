bf2a25ae72c26003cf34af7d31116ad6fcce172a
Tom Valk <tomvalk@gmail.com>
2016-01-06 14:54:26 +0100

    Entity Query where builder.

--- system/ORM/Query.php
+++ system/ORM/Query.php
@@ -8,6 +8,7 @@
  */
 
 namespace Nova\ORM;
+use Nova\ORM\Annotation\Column;
 
 /**
  * Class Query, Building a Query for finding entities.
@@ -19,10 +20,16 @@ class Query
     /** @var null|string Entity Class Name (full, with namespace) */
     private $entityClass = null;
 
+    /** @var array<string, Column>|null */
+    private $columnData = null;
+
     // Holding the query parts:
     private $where = array();
     private $limit = null;
     private $offset = null;
+
+    // Error stack
+    private $lastException = null;
 
 
     /**
@@ -39,6 +46,78 @@ class Query
         }
 
         $this->entityClass = $class->getName();
+
+        // Get column data and index it with column name in the key. Used for validating later on.
+        $this->columnData = array();
+        $columns = Structure::getTableColumns($this->entityClass);
+        foreach($columns as $column) {
+            $this->columnData[$column->name] = $column;
+        }
+    }
+
+    /**
+     * Add where conditions, you can give the conditions in multiple styles:
+     * 1. -> where('id', 1)                    for id= 1 condition
+     * 2. -> where('id', '=', 1)               for id = 1 condition
+     * 3. -> where('id', 'IN', array(1, 2))    for id IN (1,2) condition
+     * 4. -> where(array('id' => 1))           same as first style
+     * 5. -> where(array('id' => array('=' => 1)) same as second style
+     *
+     * @param string|array $criteria String with column name, or array with condition for full where syntax
+     *
+     * @param string|null $operator Only when using column name in first parameter, fill this by the value when comparing
+     * or fill in the operator used to compare
+     *
+     * @param string|null $value Only when using column name in first parameter and filling in the operator value.
+     *
+     * @return Query $this Return chained query.
+     */
+    public function where($criteria, $operator = null, $value = null)
+    {
+        // If the operator is the value, then we are going to use the = operator
+        if (! is_array($criteria) && $value === null && $this->validValue($operator, "=")) {
+            // Operator is now value!
+            $criteria = array($criteria => array("=" => $operator));
+        }
+
+        // If it's the shorthand of the where, convert it to the normal criteria.
+        if (! is_array($criteria) && $this->validOperator($operator) && $this->validValue($value, $operator)) {
+            $criteria = array($criteria => array($operator => $value));
+        }
+
+        // Get column names of table
+        $columns = Structure::getTableColumns($this->entityClass);
+
+        // Parse criteria, validate and add to the current where clause.
+        foreach ($criteria as $column => $compare) {
+            // If using shorthand for = compare
+            if (! is_array($compare)) {
+                $criteria[$column] = array('=' => $compare);
+            }
+
+            // Validate compare, validate column name
+            if (! isset($this->columnData[$column])) {
+                $this->lastException = new \Exception("Trying to prepare a where with column condition for a undefined column!", 0, $this->lastException);
+                continue;
+            }
+
+            $operator = array_keys($compare);
+            $operator = $operator[0];
+            $value = $compare[$operator];
+
+            if ($this->validOperator($operator) && $this->validValue($value, $operator)) {
+                // Add to the Query Where stack
+                $this->where[] = array(
+                    'column' => $column,
+                    'operator' => $operator,
+                    'value' => $value
+                );
+            }
+            // Skip if not valid.
+        }
+
+        var_dump($this->where);
+        return $this;
     }
 
 

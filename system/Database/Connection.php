555cfd4f1cc29d64b18e6a6d4746dcfb9dcc4aa0
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-01-07 20:32:13 +0100

    Implementing into \Nova\Database\Connection the method: fetchAll

--- system/Database/Connection.php
+++ system/Database/Connection.php
@@ -294,6 +294,20 @@ abstract class Connection extends PDO
     }
 
     /**
+     * Prepares and executes an SQL query and returns the result as an associative array.
+     *
+     * @param string $sql    The SQL query.
+     * @param array  $params The query parameters.
+     * @param array  $types  The query parameter types.
+     *
+     * @return array
+     */
+    public function fetchAll($sql, array $params = array(), $paramTypes = array(), $returnType = null)
+    {
+        return $this->select($sql, $params, true, $returnType, $paramTypes);
+    }
+
+    /**
      * Execute Select Query, bind values into the $sql query. And give optional method and class for fetch result
      * The result MUST be an array!
      *

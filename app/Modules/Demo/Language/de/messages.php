d93b705fbcfbb67d09c08300c7b52e3b9c1177d0
Martin Hering <nekomajin@gmx.de>
2016-01-07 12:08:48 +0100

    3.0 update german language file

--- app/Modules/Demo/Language/de/messages.php
+++ app/Modules/Demo/Language/de/messages.php
@@ -1,17 +1,17 @@
 <?php
 
 return array (
-  'Classic BaseModel' => '',
+  'Classic BaseModel' => 'klassisches Basis Model',
   'Classic Controller' => 'klassischer Controller',
   'Controllers' => 'Controller',
-  'DBAL' => '',
-  'DBAL BaseModel' => '',
+  'DBAL' => 'DBAL',
+  'DBAL BaseModel' => 'DBAL Basis Model',
   'Dashboard' => 'Übersicht',
   'Database' => 'Datenbank',
   'Database Abstraction Layer Demo' => 'Datenbankabstraktionsschicht (DBAL) Demo',
-  'Engine MySQL' => '',
-  'Engine SQLite' => '',
-  'Events' => '',
+  'Engine MySQL' => 'MySQL Engine',
+  'Engine SQLite' => 'SQLite Engine',
+  'Events' => 'Events',
   'Hello, welcome from the welcome controller and subpage method! <br/>
 This content can be changed in <code>{0}</code>' => 'Hallo und Willkommen im welcome-Controller in der subpage-Methode! <br />
 Dieser Inhalt kann in <code>{0}</code> geändert werden.',
@@ -19,12 +19,12 @@ Dieser Inhalt kann in <code>{0}</code> geändert werden.',
 This content can be changed in <code>{0}</code>' => 'Hallo und Willkommen im welcome-Controller! <br />
 Dieser Inhalt kann in <code>{0}</code> geändert werden.',
   'Home' => 'Home',
-  'Models' => '',
-  'MySQL Export' => '',
+  'Models' => 'Models',
+  'MySQL Export' => 'MySQL Export',
   'Open subpage' => 'Subpage öffnen',
-  'Service MySQL' => '',
-  'Service SQLite' => '',
+  'Service MySQL' => 'MySQL Service',
+  'Service SQLite' => 'SQLite Service',
   'Subpage' => 'Subpage',
-  'Themed Controller' => '',
+  'Themed Controller' => 'thematischer Controller',
   'Welcome' => 'Willkommen',
 );

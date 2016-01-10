b2755f56d8e102e05f6848633134555042a116fa
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-01-10 02:54:36 +0100

    Fix the Tests

--- scripts/testing_mysql.sql
+++ scripts/testing_mysql.sql
@@ -5,23 +5,23 @@ CREATE DATABASE IF NOT EXISTS `testdb1` DEFAULT CHARACTER SET utf8 COLLATE utf8_
 
 USE `testdb1`;
 
-DROP TABLE IF EXISTS `smvc_car`;
+DROP TABLE IF EXISTS `nova_car`;
 
-CREATE TABLE IF NOT EXISTS `smvc_car` (
+CREATE TABLE IF NOT EXISTS `nova_car` (
   `carid` int(11) NOT NULL,
   `make` varchar(45) NOT NULL,
   `model` varchar(90) NOT NULL,
   `costs` double NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-INSERT INTO `smvc_car` (`carid`, `make`, `model`, `costs`) VALUES
+INSERT INTO `nova_car` (`carid`, `make`, `model`, `costs`) VALUES
 (1, 'Tesla', 'Model S', 97000),
 (2, 'BMW', 'i8', 138000);
 
 
-ALTER TABLE `smvc_car`
+ALTER TABLE `nova_car`
   ADD PRIMARY KEY (`carid`);
 
 
-ALTER TABLE `smvc_car`
+ALTER TABLE `nova_car`
   MODIFY `carid` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;

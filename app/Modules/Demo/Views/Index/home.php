1f9f791f0b8335cba80d6fae83dd19a9c8608f40
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-01-16 13:34:20 +0100

    Cleanup into app/Modules/Demo/Views/Index/home.php

--- app/Modules/Demo/Views/Index/home.php
+++ app/Modules/Demo/Views/Index/home.php
@@ -37,33 +37,4 @@
             </div>
         </div>
     </div>
-    <!--
-    <div class="col-sm-4">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title">Database Engine & Service</h3>
-            </div>
-            <div class="panel-body">
-                Demo on Database Engine and the optional DBAL (Database Abstraction Layer) usage with entities. The MySQL demos require you to import the test database.
-            </div>
-            <div class="list-group">
-                <a href="/demos/database/mysqlexport" class="list-group-item list-group-item-info">
-                    <i class="fa fa-database"></i> Import Test Databases (MySQL)
-                </a>
-                <a href="/demos/database/engine/basic/mysql" class="list-group-item">
-                    <i class="fa fa-cubes"></i> Basic Engine Demo (MySQL)
-                </a>
-                <a href="/demos/database/engine/basic/sqlite" class="list-group-item">
-                    <i class="fa fa-cubes"></i> Basic Engine Demo (SQLite)
-                </a>
-                <a href="/demos/database/service/basic/mysql" class="list-group-item">
-                    <i class="fa fa-cubes"></i> Basic Engine DBAL Demo (MySQL)
-                </a>
-                <a href="/demos/database/service/basic/sqlite" class="list-group-item">
-                    <i class="fa fa-cubes"></i> Basic Engine DBAL Demo (SQLite)
-                </a>
-            </div>
-        </div>
-    </div>
-    -->
 </div>

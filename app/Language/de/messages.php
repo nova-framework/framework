03dddb6a5513cc0efea7538fd01660bcd5c4f32c
Martin Hering <nekomajin@gmx.de>
2016-01-25 12:10:05 +0100

    3.0 update app/Language/de/messages.php

--- app/Language/de/messages.php
+++ app/Language/de/messages.php
@@ -1,7 +1,7 @@
 <?php
 
 return array (
-  'Cannot initialize the Table Fields' => '',
+  'Cannot initialize the Table Fields' => 'Tabellenfelder können nicht initialisiert werden',
   'Hello, welcome from the welcome controller and subpage method! <br/>
 This content can be changed in <code>{0}</code>' => 'Hallo und Willkommen im welcome-Controller in der subpage-Methode! <br />
 Dieser Inhalt kann in <code>{0}</code> geändert werden.',
@@ -9,13 +9,13 @@ Dieser Inhalt kann in <code>{0}</code> geändert werden.',
 This content can be changed in <code>{0}</code>' => 'Hallo und Willkommen im welcome-Controller! <br />
 Dieser Inhalt kann in <code>{0}</code> geändert werden.',
   'Home' => 'Home',
-  'Invalid parameter' => '',
-  'Invalid parameters' => '',
-  'No valid Model Class is given: {0}' => '',
-  'No valid Model Name is given: {0}' => '',
+  'Invalid parameter' => 'Ungültiger Parameter',
+  'Invalid parameters' => 'Ungültige Parameter',
+  'No valid Model Class is given: {0}' => 'Ungültige Model Klasse: {0}',
+  'No valid Model Name is given: {0}' => 'Ungültiger Model Name: {0}',
   'Open subpage' => 'Subpage öffnen',
-  'Parameter should be a non empty Array' => '',
-  'Parameter should be an Integer' => '',
+  'Parameter should be a non empty Array' => 'Parameter sollte ein nicht leeres Array sein',
+  'Parameter should be an Integer' => 'Parameter sollte ein Integer sein',
   'Subpage' => 'Subpage',
   'Welcome' => 'Willkommen',
 );

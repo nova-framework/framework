91a867f360eb7ae177b5cbd3fa4531d9bbe4e9e6
David Carr <dave@daveismyname.com>
2016-03-26 10:16:00 +0000

    Added files via upload

---
+++ app/Views/Welcome/Welcome.php
@@ -0,0 +1,9 @@
+<div class="page-header">
+	<h1><?=$title;?></h1>
+</div>
+
+<p><?=$welcomeMessage;?></p>
+
+<a class="btn btn-md btn-success" href="<?=DIR;?>subpage">
+	<?php echo Language::show('openSubPage', 'Welcome'); ?>
+</a>

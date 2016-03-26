be766900e3e2f146d942ccdf09cd4f7f875e5373
David Carr <d.carr@theonepoint.co.uk>
2016-03-26 10:35:51 +0000

    resetting

--- app/Views/Welcome/SubPage.php
+++
@@ -1,9 +0,0 @@
-<div class="page-header">
-	<h1><?=$title;?></h1>
-</div>
-
-<p><?=$welcomeMessage;?></p>
-
-<a class="btn btn-md btn-success" href="<?=DIR;?>">
-	<?php echo Language::show('backHome', 'Welcome'); ?>
-</a>

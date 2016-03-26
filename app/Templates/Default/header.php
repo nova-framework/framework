b1cc07843eb4b826085be4b93a77385f018b6fcb
David Carr <d.carr@theonepoint.co.uk>
2016-03-26 16:07:32 +0000

    Updated default template.

--- app/Templates/Default/header.php
+++ app/Templates/Default/header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="<?php echo LANGUAGE_CODE; ?>">
 <head>
-	<meta charset="utf-8">
-	<title><?php echo $title.' - '.SITETITLE;?></title>
-	<?php
+    <meta charset="utf-8">
+    <title><?php echo $title.' - '.SITETITLE;?></title>
+    <?php
     echo $meta;//place to pass data / plugable hook zone
     Assets::css([
         'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
@@ -16,3 +16,5 @@
 <?php echo $afterBody; //place to pass data / plugable hook zone?>
 
 <div class="container">
+
+<p><img src='<?=Url::templatePath();?>images/nova.png' alt='<?=SITETITLE;?>'></p>

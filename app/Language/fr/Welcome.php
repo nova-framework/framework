6111684d72dc29d9de3c5bee35a0336954e4ecbe
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-03-26 13:10:51 +0100

    Initial commit of Language/Logs paths

---
+++ app/Language/fr/Welcome.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Sample language
+ */
+return array(
+
+	// Index method
+	'welcomeText' => 'Bienvenue',
+	'welcomeMessage' => '
+		Bonjour, bienvenue dans le contrôleur de bienvenue ! <br/>
+		Ce contenu peut être changé via le fichier: <code>app/views/welcome/welcome.php</code>
+	',
+
+	// Subpage method
+	'subpageText' => 'Sous-page',
+	'subpageMessage' => '
+		Bonjour, bienvenue dans le contrôleur de bienvenue et sa sous-page ! <br/>
+		Ce contenu peut être changé via le fichier: <code>app/views/welcome/subpage.php</code>
+	',
+
+	// Buttons
+	'openSubpage' => 'Ouvrir la sous-page',
+	'backHome' => 'Accueil',
+
+);
---
+++ app/Language/ru/Welcome.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Sample language
+ */
+return array(
+
+	// Index method
+	'welcomeText' => 'Добро пожаловать!',
+	'welcomeMessage' => '
+		Здраствуй, добро пожаловать в привствующий контролер! <br/>
+		Содержимое этого файла может быть изменено в <code>app/views/welcome/welcome.php</code>
+	',
+
+	// Subpage method
+	'subpageText' => 'Подстраница',
+	'subpageMessage' => '
+		Здраствуй, добро пожаловать в привствующий контролер в метод подстраницы! <br/>
+		Содержимое этого файла можеть быть изменено в <code>app/views/welcome/subpage.php</code>
+	',
+
+	// Buttons
+	'openSubpage' => 'Открыть подстраницу',
+	'backHome' => 'Домой',
+
+);

c8813edd6f460eaa26ea32f375bb01286be676a7
David Carr <d.carr@theonepoint.co.uk>
2016-03-26 10:31:56 +0000

    case sensitive files clash

--- app/views/welcome/welcome.php
+++
@@ -1,9 +0,0 @@
-<div class="page-header">
-	<h1><?=$title;?></h1>
-</div>
-
-<p><?=$welcomeMessage;?></p>
-
-<a class="btn btn-md btn-success" href="<?=DIR;?>subpage">
-	<?php echo Language::show('openSubPage', 'Welcome'); ?>
-</a>

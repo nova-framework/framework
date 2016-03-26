9e0f3d81e5d615555bba21ada4e39107ef818654
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-03-26 13:27:18 +0100

    Ajdust the Languages path

---
+++ app/Language/It/Welcome.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Sample language
+ */
+return array(
+
+	// Index method
+	'welcomeText' => 'Benvenuto',
+	'welcomeMessage' => '
+		Ciao, benvenuto dal controller di benvenuto! <br/>
+		Questo contenuto può essere cambiato in <code>app/views/welcome/welcome.php</code>
+	',
+
+	// Subpage method
+	'subpageText' => 'Sottopagina',
+	'subpageMessage' => '
+		Ciao, benvenuto dal controller di benvenuto e dal metodo sottopagina! <br/>
+		Questo contenuto può essere cambiato in <code>app/views/welcome/subpage.php</code>
+	',
+
+	// Buttons
+	'openSubpage' => 'Apri sottopagina',
+	'backHome' => 'Home',
+
+);
---
+++ app/Language/Nl/Welcome.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Sample language
+ */
+return array(
+
+	// Index method
+	'welcomeText' => 'Welkom',
+	'welcomeMessage' => '
+		Hallo, welkom van de welcome controller! <br/>
+		Deze inhoud kan worden veranderd in <code>app/views/welcome/welcome.php</code>
+	',
+
+	// Subpage method
+	'subpageText' => 'Subpagina',
+	'subpageMessage' => '
+		Hallo, welkom van de welcome controller en de subpagina methode! <br/>
+		Deze inhoud kan worden veranderd in <code>app/views/welcome/subpage.php</code>
+	',
+
+	// Buttons
+	'openSubpage' => 'Open subpagina',
+	'backHome' => 'Home',
+
+);

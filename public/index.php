7eee53b14ae0c75a4179d4e978846d7ac794cccf
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-04-10 23:27:07 +0200

    Use DS for better operability into paths definition

--- public/index.php
+++ public/index.php
@@ -4,10 +4,10 @@ defined('DS') || define('DS', DIRECTORY_SEPARATOR);
 
 /** Define the absolute paths for configured directories */
 
-define('APPDIR', realpath(__DIR__.'/../app/').'/');
-define('SYSTEMDIR', realpath(__DIR__.'/../system/').'/');
-define('PUBLICDIR', realpath(__DIR__).'/');
-define('ROOTDIR', realpath(__DIR__.'/../').'/');
+define('APPDIR', realpath(__DIR__.'/../app/') .DS);
+define('SYSTEMDIR', realpath(__DIR__.'/../system/') .DS);
+define('PUBLICDIR', realpath(__DIR__) .DS);
+define('ROOTDIR', realpath(__DIR__.'/../') .DS);
 
 /** load composer autoloader */
 if (file_exists(ROOTDIR.'vendor/autoload.php')) {
@@ -65,4 +65,4 @@ if (defined('ENVIRONMENT')) {
 }
 
 /** Bootstrap the Framework */
-require SYSTEMDIR.'bootstrap.php';
+require SYSTEMDIR .'bootstrap.php';

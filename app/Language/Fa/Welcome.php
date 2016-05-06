09d35d71b26811cb2130e9767a0e8f852b2f47f8
Amirhosein Loghat <amiroperator@gmail.com>
2016-05-06 19:47:40 +0430

    Persian Language
    adding persiang language to nova-framework

---
+++ app/Language/Fa/Welcome.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Persian language
+ */
+return [
+	// Index method
+	'welcomeText' => 'خوش آمدید',
+	'welcomeMessage' => '
+		درود, به کنترل‎‎گر صفحه خوش آمد گویی خوش آمدید! <br/>
+		محتوای این صفحه میتواند در فایل <code>/app/Views/Welcome/Welcome.php</code> ویرایش شود.
+	',
+
+	// Subpage method
+	'subpageText' => 'زیر‎صفحه',
+	'subpageMessage' => '
+		درود, به کنترل‎گر صفحه خوش آمد گویی و متد زیر‎صفحه خوش آمدید! <br/>
+		محتوای این صفحه میتواند در فایل <code>/app/Views/Welcome/SubPage.php</code> ویرایش شود.
+	',
+
+	// Buttons
+	'openSubPage' => 'بازکردن زیر‎صفحه',
+	'backHome' => 'خانه',
+];

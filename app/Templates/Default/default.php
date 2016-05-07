8451d53a5d6af03f58c5108e9a94336a7b21a98a
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-05-07 13:12:14 +0200

    Small change into default Layout

--- app/Templates/Default/default.php
+++ app/Templates/Default/default.php
@@ -24,17 +24,17 @@ echo $css; //place to pass data / plugable hook zone
 
 <div class="container">
     <p class="pull-right">
-        <a href='<?=DIR;?>language/cs'>Czech</a> |
-        <a href='<?=DIR;?>language/en'>English</a> |
-        <a href='<?=DIR;?>language/de'>German</a> |
-        <a href='<?=DIR;?>language/fr'>French</a> |
-        <a href='<?=DIR;?>language/it'>Italian</a> |
-        <a href='<?=DIR;?>language/nl'>Dutch</a> |
-        <a href='<?=DIR;?>language/fa'>Persian</a> |
-        <a href='<?=DIR;?>language/pl'>Polish</a> |
-        <a href='<?=DIR;?>language/ro'>Romanian</a> |
-        <a href='<?=DIR;?>language/ru'>Russian</a> |
-        <a href='<?=DIR;?>language/es'>Spanish</a>
+        <a href='<?= DIR; ?>language/cs'>Czech</a> |
+        <a href='<?= DIR; ?>language/en'>English</a> |
+        <a href='<?= DIR; ?>language/de'>German</a> |
+        <a href='<?= DIR; ?>language/fr'>French</a> |
+        <a href='<?= DIR; ?>language/it'>Italian</a> |
+        <a href='<?= DIR; ?>language/nl'>Dutch</a> |
+        <a href='<?= DIR; ?>language/fa'>Persian</a> |
+        <a href='<?= DIR; ?>language/pl'>Polish</a> |
+        <a href='<?= DIR; ?>language/ro'>Romanian</a> |
+        <a href='<?= DIR; ?>language/ru'>Russian</a> |
+        <a href='<?= DIR; ?>language/es'>Spanish</a>
     </p>
     <div class="clearfix"></div>
     <p>

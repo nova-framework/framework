13e5e173be3ad95f2c23864cdd13712f58b43a13
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-05-11 21:23:48 +0200

    Introduce a Navbar into Custom Layout

--- app/Templates/Default/custom.php
+++ app/Templates/Default/custom.php
@@ -13,26 +13,44 @@ echo $meta; //place to pass data / plugable hook zone
 
 Assets::css([
     'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css',
+    'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css',
     template_url('css/style.css', 'Default'),
 ]);
 
 echo $css; //place to pass data / plugable hook zone
 ?>
 </head>
-<body>
+<body style='padding-top: 60px;'>
+
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?= site_url('dashboard'); ?>"><strong><?= SITETITLE; ?></strong></a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right" style="margin-right: 5px;">
+                <?php if (Auth::check()) { ?>
+                <li>
+                    <a href='<?= site_url('logout'); ?>'><i class='fa fa-sign-out'></i> Logout</a>
+                </li>
+                <?php } ?>
+            </ul>
+        </div>
+    </div>
+</nav>
+
 <?= $afterBody; //place to pass data / plugable hook zone ?>
 
 <div class="container">
-    <?php if (Auth::check()) { ?>
-    <p class="pull-right" style="margin-top: 10px;">
-        <a class="btn btn-sm btn-primary" href='<?= site_url('logout'); ?>'>Logout</a>
-    </p>
-    <div class="clearfix"></div>
-    <?php } ?>
-    <p>
-        <img src='<?= template_url('images/nova.png', 'Default'); ?>' alt='<?= SITETITLE; ?>'>
-    </p>
-    
+    <p><img src='<?= template_url('images/nova.png', 'Default'); ?>' alt='<?= SITETITLE; ?>'></p>
+
     <?= $content; ?>
 </div>
 

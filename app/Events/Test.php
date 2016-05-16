3ed1481aa6b0469b3dbaf289322c642e40cf0e6b
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-05-16 12:12:17 +0200

    Improve the App\Events\Test@handle

--- app/Events/Test.php
+++ app/Events/Test.php
@@ -6,6 +6,6 @@ class Test
 {
     public static function handle($data)
     {
-        return '<pre>App\Events\Test@handle : ' .var_export($data, true) .'</pre>';
+        return '<pre>' .__METHOD__ .' : ' .var_export($data, true) .'</pre>';
     }
 }

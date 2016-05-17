6ec0f10c14a6f16317b50d50701f2fc4d95bec00
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-05-17 20:11:18 +0200

    Initial implementation of the Demos Module

---
+++ app/Modules/Demos/Config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Config - the Module's specific Configuration.
+ *
+ * @author David Carr - dave@daveismyname.com
+ * @author Edwin Hoksberg - info@edwinhoksberg.nl
+ * @version 3.0
+ */
+
+use Core\Config;
+
+/**
+ * Configuration constants and options.
+ */
---
+++ app/Modules/Demos/Routes.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Routes - all Module's specific Routes are defined here.
+ *
+ * @author Virgil-Adrian Teaca - virgil@giulianaeassociati.com
+ * @version 3.0
+ */
+
+use Core\Router;
+
+
+/** Define static routes. */
+y

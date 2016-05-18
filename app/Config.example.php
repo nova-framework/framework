4e2fbe8b5ca96796629784cabcd63d69b0d6fce6
Amirhosein Loghat <amiroperator@gmail.com>
2016-05-19 00:10:16 +0430

    Update Config.example.php

--- app/Config.example.php
+++ app/Config.example.php
@@ -111,18 +111,18 @@ define('CACHEPATH', APPDIR .'Cache');
  * All known Languages
  */
 Config::set('languages', array(
-    'cz' => array('info' => 'Czech',     'name' => 'čeština',    'locale' => 'cz_CZ', 'direction' => 'ltr'),
-    'de' => array('info' => 'German',    'name' => 'Deutsch',    'locale' => 'de_DE', 'direction' => 'ltr'),
-    'en' => array('info' => 'English',   'name' => 'English',    'locale' => 'en_US', 'direction' => 'ltr'),
-    'es' => array('info' => 'Spanish',   'name' => 'Español',    'locale' => 'es_ES', 'direction' => 'ltr'),
-    'fa' => array('info' => 'Persian',   'name' => 'پارسی',      'locale' => 'fa_IR', 'direction' => 'rtl'),
-    'fr' => array('info' => 'French',    'name' => 'Français',   'locale' => 'fr_FR', 'direction' => 'ltr'),
-    'it' => array('info' => 'Italian',   'name' => 'italiano',   'locale' => 'it_IT', 'direction' => 'ltr'),
-    'ja' => array('info' => 'Japanesse', 'name' => '日本語',      'locale' => 'ja_JA', 'direction' => 'ltr'),
-    'nl' => array('info' => 'Dutch',     'name' => 'Nederlands', 'locale' => 'nl_NL', 'direction' => 'ltr'),
-    'pl' => array('info' => 'Polish',    'name' => 'polski',     'locale' => 'pl_PL', 'direction' => 'ltr'),
-    'ro' => array('info' => 'Romanian',  'name' => 'Română',     'locale' => 'ro_RO', 'direction' => 'ltr'),
-    'ru' => array('info' => 'Russian',   'name' => 'ру́сский',    'locale' => 'ru_RU', 'direction' => 'ltr'),
+    'cz' => array('info' => 'Czech',     'name' => 'čeština',    'locale' => 'cz_CZ', 'dir' => 'ltr'),
+    'de' => array('info' => 'German',    'name' => 'Deutsch',    'locale' => 'de_DE', 'dir' => 'ltr'),
+    'en' => array('info' => 'English',   'name' => 'English',    'locale' => 'en_US', 'dir' => 'ltr'),
+    'es' => array('info' => 'Spanish',   'name' => 'Español',    'locale' => 'es_ES', 'dir' => 'ltr'),
+    'fa' => array('info' => 'Persian',   'name' => 'پارسی',      'locale' => 'fa_IR', 'dir' => 'rtl'),
+    'fr' => array('info' => 'French',    'name' => 'Français',   'locale' => 'fr_FR', 'dir' => 'ltr'),
+    'it' => array('info' => 'Italian',   'name' => 'italiano',   'locale' => 'it_IT', 'dir' => 'ltr'),
+    'ja' => array('info' => 'Japanesse', 'name' => '日本語',      'locale' => 'ja_JA', 'dir' => 'ltr'),
+    'nl' => array('info' => 'Dutch',     'name' => 'Nederlands', 'locale' => 'nl_NL', 'dir' => 'ltr'),
+    'pl' => array('info' => 'Polish',    'name' => 'polski',     'locale' => 'pl_PL', 'dir' => 'ltr'),
+    'ro' => array('info' => 'Romanian',  'name' => 'Română',     'locale' => 'ro_RO', 'dir' => 'ltr'),
+    'ru' => array('info' => 'Russian',   'name' => 'ру́сский',    'locale' => 'ru_RU', 'dir' => 'ltr'),
 ));
 
 /**

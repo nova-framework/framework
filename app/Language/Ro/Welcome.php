f7803652dc8cf5b562d3b483435268666d299bd4
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-05-19 18:54:56 +0200

    Adjust Romanian translation for App\Controllers\Welcome

--- app/Language/Ro/Welcome.php
+++ app/Language/Ro/Welcome.php
@@ -6,13 +6,13 @@ return array(
 	// Metoda 'index'
 	'welcomeText' => 'Bun venit',
 	'welcomeMessage' => '
-		Bună, bun venit la controlerul <code>welcome</code>! <br/>
+		Bună, bun venit la controlerul <code>Welcome</code>! <br/>
 		Acest conținut poate fi modificat în <code>/app/Views/Welcome/Welcome.php</code>
 	',
 	// Metoda 'subpage'
 	'subpageText' => 'Subpagina',
 	'subpageMessage' => '
-		Bună, bun venit la controlerul <code>welcome</code> și metoda <code>subpage</code>! <br/>
+		Bună, bun venit la controlerul <code>Welcome</code> și metoda <code>subPage</code>! <br/>
 		Acest conținut poate fi modificat în <code>/app/Views/Welcome/SubPage.php</code>
 	',
 	// Butoane

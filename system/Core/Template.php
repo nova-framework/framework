32fe159269ac82957a5f11528b5baf54cf09ac35
Amir Operator <amiroperator@gmail.com>
2016-05-19 18:52:56 +0430

    Update Core\Template.php

--- system/Core/Template.php
+++ system/Core/Template.php
@@ -33,19 +33,22 @@ class Template extends BaseView
      * Create a Template instance
      *
      * @param string $view
-<<<<<<< HEAD
-     * @param array $data
-     * @param string $template
-=======
      * @param array|string $data
      * @param string $custom
->>>>>>> 4f41a337ead7c53ca9ff3815790d14b9134b44bc
      * @return Template
      */
     public static function make($view, $data = array(), $template = TEMPLATE)
     {
-<<<<<<< HEAD
-        $direction = CoreLanguage::direction();
+        if (is_string($data)) {
+            if (! empty($data) && ($template === null)) {
+                // The Module name given as second parameter; adjust the information.
+                $template = $data;
+            }
+
+            $data = array();
+        }
+
+		$direction = CoreLanguage::direction();
 
         $ltrPath = str_replace('/', DS, APPDIR ."Templates/$template/$view.php");
         $rtlPath = str_replace('/', DS, APPDIR ."Templates/$template/$view-rtl.php");
@@ -56,20 +59,7 @@ class Template extends BaseView
         //Prepare the file path if exists for rtl layouts
         if (file_exists($rtlPath) && $direction == 'rtl')
             $path = $rtlPath;
-=======
-        if (is_string($data)) {
-            if (! empty($data) && ($template === null)) {
-                // The Module name given as second parameter; adjust the information.
-                $template = $data;
-            }
-
-            $data = array();
-        }
-
-        // Prepare the file path.
-        $path = str_replace('/', DS, APPDIR ."Templates/$template/$view.php");
->>>>>>> 4f41a337ead7c53ca9ff3815790d14b9134b44bc
-
+		
         return new Template($view, $path, $data);
     }
 }

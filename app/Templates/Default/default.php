0d2a1d059ac0ca7005960af4828d59233fa16e6a
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-05-20 19:51:11 +0200

    Simplify the Defaut Layout and the Header from Default Template

--- app/Templates/Default/default.php
+++ app/Templates/Default/default.php
@@ -45,15 +45,6 @@ echo $css; // Place to pass data / plugable hook zone
 
 <nav class="navbar navbar-default navbar-xs navbar-fixed-top" role="navigation">
     <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav navbar-right" style="margin-right: 5px;">
                 <?= $langMenu; ?>

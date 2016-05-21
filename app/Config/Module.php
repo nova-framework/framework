ec00fbabfd43201485941553e1e32818f133a761
Amir Operator <amiroperator@gmail.com>
2016-05-21 18:28:36 +0430

    Added missing author in Module Config

--- app/Config/Module.php
+++ app/Config/Module.php
@@ -4,6 +4,7 @@
  *
  * @author David Carr - dave@daveismyname.com
  * @author Edwin Hoksberg - info@edwinhoksberg.nl
+ * @author Virgil-Adrian Teaca - virgil@giulianaeassociati.com
  * @version 3.0
  */
 

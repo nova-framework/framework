a0ca29d279858347132ba4a1f616dbdc4c641720
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-05-23 10:21:29 +0200

    Redirect to previous page after changing the Language

--- app/Controllers/Language.php
+++ app/Controllers/Language.php
@@ -35,6 +35,6 @@ class Language extends Controller
             Cookie::set(PREFIX .'language', $language);
         }
 
-        return Redirect::to('');
+        return Redirect::back();
     }
 }

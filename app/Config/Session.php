5df76252180c26a73c2327ed2aa88ff455818a00
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-05-24 22:35:02 +0200

    Small changes on app/Config/Session.php

--- app/Config/Session.php
+++ app/Config/Session.php
@@ -10,12 +10,12 @@ use Core\Config;
 
 
 Config::set('session', array(
-    'handler'  => '\Session\FileSessionHandler', // Default Session Handler, using files for Session caching.
+    'handler' => '\Session\FileSessionHandler', // The default Session Handler, using files for Session cache.
 
     // Storage configuration.
-    'lifetime' => 180,          // Number of minutes the Session is allowed to remain idle before it expires.
-    'files'    => SESSION_PATH, // File Session Handler - the location where the Session files may be stored.
-    'lottery' => array(2, 100), // Option used by the Garbage Collector, to remove the stalled Session files.
+    'lifetime' => 180,           // Number of minutes the Session is allowed to remain idle before it expires.
+    'files'    => SESSION_PATH,  // File Session Handler - the location where the Session files may be stored.
+    'lottery'  => array(2, 100), // Option used by the Garbage Collector, to remove the stalled Session files.
 
     // Cookie configuration.
     'name'     => PREFIX .'session',

90bbf1ef4d8ef04944a2141eaf84a95ca5d71aa3
Amir Operator <amiroperator@gmail.com>
2016-05-26 16:16:19 +0430

    Minify html page output

--- system/functions.php
+++ system/functions.php
@@ -387,3 +387,28 @@ function createKey($length = 32)
 {
     return str_random($length);
 }
+
+/**
+ * Remove white-spaces from the HTML
+ * @param string $buffer
+ * @return string
+ */
+function sanitize_output($buffer) 
+{
+
+    $search = array(
+        '/\>[^\S ]+/s',  // strip whitespaces after tags, except space
+        '/[^\S ]+\</s',  // strip whitespaces before tags, except space
+        '/(\s)+/s'       // shorten multiple whitespace sequences
+    );
+
+    $replace = array(
+        '>',
+        '<',
+        '\\1'
+    );
+
+    $buffer = preg_replace($search, $replace, $buffer);
+
+    return $buffer;
+}

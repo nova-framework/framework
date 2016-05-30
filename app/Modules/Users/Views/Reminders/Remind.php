b313ac8853443c06b9c077826580cd512e048235
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-05-30 13:05:00 +0200

    Adjust app/Modules/Users/Views/Reminders/Remind.php

--- app/Modules/Users/Views/Reminders/Remind.php
+++ app/Modules/Users/Views/Reminders/Remind.php
@@ -22,7 +22,11 @@
                     <div class="form-group">
                         <p><input type="email" name="email" id="email" class="form-control input-lg col-xs-12 col-sm-12 col-md-12" placeholder="<?= __d('users', 'E-mail'); ?>"><br><br></p>
                     </div>
-
+                    <div class="row pull-right" style="margin-top: 10px; margin-right: 0;">
+                        <div class="g-recaptcha" data-sitekey="<?= Config::get('recaptcha.siteKey'); ?>"></div>
+                    </div>
+                    <div class="clearfix"></div>
+                    <hr>
                     <div class="row" style="margin-top: 22px;">
                         <div class="col-xs-6 col-sm-6 col-md-6">
                             <input type="submit" name="submit" class="btn btn-success col-sm-10" value="<?= __d('users', 'Send Reset Link'); ?>">
@@ -30,9 +34,6 @@
                         <div class="col-xs-6 col-sm-6 col-md-6">
                             <a href="<?= site_url('login'); ?>" class="btn btn-link pull-right"><?= __d('users', 'Login'); ?></a>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="g-recaptcha" data-sitekey="<?= Config::get('recaptcha.siteKey'); ?>"></div>
                     </div>
                 </fieldset>
 

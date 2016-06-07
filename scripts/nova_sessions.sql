3cccedaecd7ef0aaf08d88c29651a1bc75db6110
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-08 01:05:56 +0200

    Update the SQL scripts

--- scripts/nova_sessions.sql
+++ scripts/nova_sessions.sql
@@ -1,9 +1,9 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.5.1
+-- version 4.5.5
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 07, 2016 at 03:39 PM
+-- Generation Time: Jun 07, 2016 at 11:02 PM
 -- Server version: 10.0.25-MariaDB
 -- PHP Version: 5.6.22
 
@@ -31,6 +31,16 @@ CREATE TABLE `nova_sessions` (
   `payload` text NOT NULL,
   `last_activity` int(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `nova_sessions`
+--
+ALTER TABLE `nova_sessions`
+  ADD UNIQUE KEY `id` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

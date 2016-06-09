1f5362ecc86379f36a51b69f16bf0e447f286f5e
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-10 01:52:03 +0200

    Small changes on Database\ConnectionResolverInterface

--- system/Database/ConnectionResolverInterface.php
+++ system/Database/ConnectionResolverInterface.php
@@ -1,28 +1,30 @@
-<?php namespace Illuminate\Database;
+<?php
 
-interface ConnectionResolverInterface {
+namespace Database;
 
-	/**
-	 * Get a database connection instance.
-	 *
-	 * @param  string  $name
-	 * @return \Illuminate\Database\Connection
-	 */
-	public function connection($name = null);
+interface ConnectionResolverInterface
+{
+    /**
+     * Get a database connection instance.
+     *
+     * @param  string  $name
+     * @return \Database\Connection
+     */
+    public function connection($name = null);
 
-	/**
-	 * Get the default connection name.
-	 *
-	 * @return string
-	 */
-	public function getDefaultConnection();
+    /**
+     * Get the default connection name.
+     *
+     * @return string
+     */
+    public function getDefaultConnection();
 
-	/**
-	 * Set the default connection name.
-	 *
-	 * @param  string  $name
-	 * @return void
-	 */
-	public function setDefaultConnection($name);
+    /**
+     * Set the default connection name.
+     *
+     * @param  string  $name
+     * @return void
+     */
+    public function setDefaultConnection($name);
 
 }

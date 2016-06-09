dc3da5946b3b387b36d9dab76e1d66762a0f5bae
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-09 20:00:46 +0200

    More logic... logic on app/Config/App.php

--- app/Config/App.php
+++ app/Config/App.php
@@ -27,75 +27,73 @@ Config::set('app', array(
         'app'     => APPDIR,
         'storage' => APPDIR .'Storage' .DS,
     ),
-));
 
-/**
- * The registered Service Providers.
- */
-Config::set('providers', array(
-    'Events\EventServiceProvider',
-    'Cookie\CookieServiceProvider',
-    'Encryption\EncryptionServiceProvider',
-    'Database\DatabaseServiceProvider',
-    'Session\SessionServiceProvider',
-    'Validation\ValidationServiceProvider',
-    'Pagination\PaginationServiceProvider',
-    'Mailer\MailerServiceProvider',
-    'Routing\RoutingServiceProvider',
-));
+    // The Encryption Key.
+    'key' => ENCRYPT_KEY,
 
-/**
- * The Class Aliases configuration.
- */
-Config::set('classAliases', array(
-    // The Core Tools
-    'Errors'        => '\Core\Error',
+    // The registered Service Providers.
+    'providers' => array(
+        'Cookie\CookieServiceProvider',
+        'Encryption\EncryptionServiceProvider',
+        'Database\DatabaseServiceProvider',
+        'Session\SessionServiceProvider',
+        'Validation\ValidationServiceProvider',
+        'Pagination\PaginationServiceProvider',
+        'Mailer\MailerServiceProvider',
+        'Routing\RoutingServiceProvider',
+    ),
 
-    // The Helpers
-    'Mail'          => '\Helpers\Mailer',
-    'Assets'        => '\Helpers\Assets',
-    'Csrf'          => '\Helpers\Csrf',
-    'Date'          => '\Helpers\Date',
-    'Document'      => '\Helpers\Document',
-    'Encrypter'     => '\Helpers\Encrypter',
-    'FastCache'     => '\Helpers\FastCache',
-    'Form'          => '\Helpers\Form',
-    'Ftp'           => '\Helpers\Ftp',
-    'GeoCode'       => '\Helpers\GeoCode',
-    'Hooks'         => '\Helpers\Hooks',
-    'Inflector'     => '\Helpers\Inflector',
-    'Number'        => '\Helpers\Number',
-    'RainCaptcha'   => '\Helpers\RainCaptcha',
-    'ReservedWords' => '\Helpers\ReservedWords',
-    'SimpleCurl'    => '\Helpers\SimpleCurl',
-    'TableBuilder'  => '\Helpers\TableBuilder',
-    'Tags'          => '\Helpers\Tags',
-    'Url'           => '\Helpers\Url',
+    // The registered Class Aliases.
+    'aliases' => array(
+        // The Core Tools
+        'Errors'        => '\Core\Error',
 
-    // Forensics
-    'Console'       => '\Forensics\Console',
+        // The Helpers
+        'Mail'          => '\Helpers\Mailer',
+        'Assets'        => '\Helpers\Assets',
+        'Csrf'          => '\Helpers\Csrf',
+        'Date'          => '\Helpers\Date',
+        'Document'      => '\Helpers\Document',
+        'Encrypter'     => '\Helpers\Encrypter',
+        'FastCache'     => '\Helpers\FastCache',
+        'Form'          => '\Helpers\Form',
+        'Ftp'           => '\Helpers\Ftp',
+        'GeoCode'       => '\Helpers\GeoCode',
+        'Hooks'         => '\Helpers\Hooks',
+        'Inflector'     => '\Helpers\Inflector',
+        'Number'        => '\Helpers\Number',
+        'RainCaptcha'   => '\Helpers\RainCaptcha',
+        'ReservedWords' => '\Helpers\ReservedWords',
+        'SimpleCurl'    => '\Helpers\SimpleCurl',
+        'TableBuilder'  => '\Helpers\TableBuilder',
+        'Tags'          => '\Helpers\Tags',
+        'Url'           => '\Helpers\Url',
 
-    // The Support Classes
-    'Arr'           => '\Support\Arr',
-    'Str'           => '\Support\Str',
+        // Forensics
+        'Console'       => '\Forensics\Console',
 
-    // The Support Facades
-    'App'           => '\Support\Facades\App',
-    'Auth'          => '\Support\Facades\Auth',
-    'Config'        => '\Support\Facades\Config',
-    'Cookie'        => '\Support\Facades\Cookie',
-    'Crypt'         => '\Support\Facades\Crypt',
-    'DB'            => '\Support\Facades\Database',
-    'Event'         => '\Support\Facades\Event',
-    'Hash'          => '\Support\Facades\Hash',
-    'Input'         => '\Support\Facades\Input',
-    'Language'      => '\Support\Facades\Language',
-    'Mailer'        => '\Support\Facades\Mailer',
-    'Paginator'     => '\Support\Facades\Paginator',
-    'Password'      => '\Support\Facades\Password',
-    'Redirect'      => '\Support\Facades\Redirect',
-    'Request'       => '\Support\Facades\Request',
-    'Response'      => '\Support\Facades\Response',
-    'Session'       => '\Support\Facades\Session',
-    'Validator'     => '\Support\Facades\Validator',
+        // The Support Classes
+        'Arr'           => '\Support\Arr',
+        'Str'           => '\Support\Str',
+
+        // The Support Facades
+        'App'           => '\Support\Facades\App',
+        'Auth'          => '\Support\Facades\Auth',
+        'Config'        => '\Support\Facades\Config',
+        'Cookie'        => '\Support\Facades\Cookie',
+        'Crypt'         => '\Support\Facades\Crypt',
+        'DB'            => '\Support\Facades\Database',
+        'Event'         => '\Support\Facades\Event',
+        'Hash'          => '\Support\Facades\Hash',
+        'Input'         => '\Support\Facades\Input',
+        'Language'      => '\Support\Facades\Language',
+        'Mailer'        => '\Support\Facades\Mailer',
+        'Paginator'     => '\Support\Facades\Paginator',
+        'Password'      => '\Support\Facades\Password',
+        'Redirect'      => '\Support\Facades\Redirect',
+        'Request'       => '\Support\Facades\Request',
+        'Response'      => '\Support\Facades\Response',
+        'Session'       => '\Support\Facades\Session',
+        'Validator'     => '\Support\Facades\Validator',
+    ),
 ));

be09085825d0d966202ef141dc98fb56a3fbaa33
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-10 12:23:04 +0200

    Improve the Auth's Database and Extended Providers

--- system/Auth/ExtendedUserProvider.php
+++ system/Auth/ExtendedUserProvider.php
@@ -76,9 +76,6 @@ class ExtendedUserProvider implements UserProviderInterface
      */
     public function retrieveByCredentials(array $credentials)
     {
-        // First we will add each credential element to the query as a where clause.
-        // Then we can execute the query and, if we found a user, return it in a
-        // Eloquent User "model" that will be utilized by the Guard instances.
         $query = $this->createModel()->newQuery();
 
         foreach ($credentials as $key => $value) {

75190e4585d4f6e5948e22f84e4b90da3f0f1e3b
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-11 13:04:48 +0200

    Small changes on public/index.php

--- public/index.php
+++ public/index.php
@@ -2,16 +2,14 @@
 
 defined('DS') || define('DS', DIRECTORY_SEPARATOR);
 
-/** Define the absolute paths for configured directories */
+/** Define the absolute paths for configured directories. */
 
 define('APPDIR', realpath(__DIR__.'/../app/') .DS);
 define('SYSTEMDIR', realpath(__DIR__.'/../system/') .DS);
 define('PUBLICDIR', realpath(__DIR__) .DS);
 define('ROOTDIR', realpath(__DIR__.'/../') .DS);
 
-/**
- * Set the Storage Path.
- */
+/** Set the Storage Path. */
 define('STORAGE_PATH', APPDIR .'Storage' .DS);
 
 /** Load the composer autoloader */

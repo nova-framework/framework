4753b4b8f37f895227f60dd33960b3da8354abb8
daveismyname <dave@daveismyname.com>
2016-06-18 20:58:39 +0100

    Corrected CLI path for make:key

--- system/Cli/EncryptionCommand.php
+++ system/Cli/EncryptionCommand.php
@@ -40,6 +40,6 @@ class EncryptionCommand extends Command
 
         $file = str_replace("    'key' => 'SomeRandomString______1234567890',", "    'key' => '$key',", $file);
 
-        file_put_contents("app/Config.php", $file);
+        file_put_contents("app/Config/App.php", $file);
     }
 }

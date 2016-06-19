612c0f45a40c9502caae4e18bffd1e49bd271dfc
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-19 23:26:58 +0200

    Add back the Composer running failure message

--- public/index.php
+++ public/index.php
@@ -25,5 +25,13 @@ define('ROOTDIR', realpath(__DIR__.'/../') .DS);
  */
 define('ENVIRONMENT', 'development');
 
+/** Composer installation check. */
+if (! file_exists(ROOTDIR .'vendor/autoload.php')) {
+    echo "<h1>Please install via composer.json</h1>";
+    echo "<p>Install Composer instructions: <a href='https://getcomposer.org/doc/00-intro.md#globally'>https://getcomposer.org/doc/00-intro.md#globally</a></p>";
+    echo "<p>Once composer is installed, navigate to the working directory in your terminal/command prompt and enter 'composer install'</p>";
+    exit;
+}
+
 /** Boot the Application. */
 require APPDIR .'Boot' .DS .'Start.php';

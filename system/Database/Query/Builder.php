4e7e4086ea8f8ac70f155ef943fe92a3d8cb83d0
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-06-25 17:40:08 +0200

    Fix the New Style Pagination

--- system/Database/Query/Builder.php
+++ system/Database/Query/Builder.php
@@ -1192,7 +1192,7 @@ class Builder
         // Once we have the total number of records to be paginated, we can grab the
         // current page and the result array. Then we are ready to create a brand
         // new Paginator instances for the results which will create the links.
-        $page = $paginator->getCurrentPage($total, $perPage);
+        $page = $paginator->getCurrentPage($total);
 
         $results = $this->forPage($page, $perPage)->get($columns);
 

8932f67291bfafb0418b5c820463a5bfbbec84b1
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-07-11 09:24:20 +0200

    Adjust the translation calls on AdminLTE Tempate and update the Language files

--- app/Templates/AdminLte/Language/Fa/messages.php
+++ app/Templates/AdminLte/Language/Fa/messages.php
@@ -1,10 +1,17 @@
 <?php
 
 return array (
+  'ADMINISTRATION' => '',
+  'Control Panel' => '',
+  'Dashboard' => '',
   'Forgot Password?' => 'فراموشی رمزعبور؟',
   'Logout' => 'خروج',
+  'Member since {0}' => '',
   'Profile' => 'پروفایل',
+  'Roles' => '',
+  'Settings' => '',
   'Sign In' => '',
   'Sign Up' => '',
-  'User Login' => 'ورود کاربر',
+  'Sign out' => '',
+  'Users' => '',
 );

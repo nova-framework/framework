704f030d754c7d765aca929f44717f82be287827
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-07-25 11:27:27 +0200

    Small change on Http\Response

--- system/Http/Response.php
+++ system/Http/Response.php
@@ -2,7 +2,7 @@
 
 namespace Http;
 
-use Core\BaseView;
+use Core\BaseView as View;
 use Support\Contracts\JsonableInterface;
 use Support\Contracts\RenderableInterface;
 
@@ -65,7 +65,7 @@ class Response extends \Symfony\Component\HttpFoundation\Response
             $content = $this->morphToJson($content);
         } else if ($content instanceof RenderableInterface) {
             $content = $content->render();
-        } else if ($content instanceof BaseView) {
+        } else if ($content instanceof View) {
             $content = $content->fetch();
         }
 

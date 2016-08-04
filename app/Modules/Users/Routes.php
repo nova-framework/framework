6682c8eb29636e1891024838d63933cc593ded3b
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-08-04 14:49:29 +0200

    Move the entire Route examples from SMVC Legacy Syntax to native one

--- app/Modules/Users/Routes.php
+++ app/Modules/Users/Routes.php
@@ -37,7 +37,7 @@ Router::post('password/remind', array(
 ));
 
 // The Password Reset.
-Router::get('password/reset(/(:any))', array(
+Router::get('password/reset/{token?}', array(
     'before' => 'guest',
     'uses'   => 'App\Modules\Users\Controllers\Authorize@reset'
 ));
@@ -58,7 +58,7 @@ Router::post('register', array(
     'uses'   => 'App\Modules\Users\Controllers\Registrar@store'
 ));
 
-Router::get('register/verify/(:any)', array(
+Router::get('register/verify/{token?}', array(
     'before' => 'guest',
     'uses'   => 'App\Modules\Users\Controllers\Registrar@verify'
 ));
@@ -75,23 +75,23 @@ Router::group(array('prefix' => 'admin', 'namespace' => 'App\Modules\Users\Contr
     Router::post('users/profile', array('before' => 'auth|csrf', 'uses' => 'Users@postProfile'));
 
     // The Users CRUD.
-    Router::get( 'users',                array('before' => 'auth',      'uses' => 'Users@index'));
-    Router::get( 'users/create',         array('before' => 'auth',      'uses' => 'Users@create'));
-    Router::post('users',                array('before' => 'auth|csrf', 'uses' => 'Users@store'));
-    Router::get( 'users/(:num)',         array('before' => 'auth',      'uses' => 'Users@show'));
-    Router::get( 'users/(:num)/edit',    array('before' => 'auth',      'uses' => 'Users@edit'));
-    Router::post('users/(:num)',         array('before' => 'auth|csrf', 'uses' => 'Users@update'));
-    Router::post('users/(:num)/destroy', array('before' => 'auth|csrf', 'uses' => 'Users@destroy'));
+    Router::get( 'users',              array('before' => 'auth',      'uses' => 'Users@index'));
+    Router::get( 'users/create',       array('before' => 'auth',      'uses' => 'Users@create'));
+    Router::post('users',              array('before' => 'auth|csrf', 'uses' => 'Users@store'));
+    Router::get( 'users/{id}',         array('before' => 'auth',      'uses' => 'Users@show'));
+    Router::get( 'users/{id}/edit',    array('before' => 'auth',      'uses' => 'Users@edit'));
+    Router::post('users/{id}',         array('before' => 'auth|csrf', 'uses' => 'Users@update'));
+    Router::post('users/{id}/destroy', array('before' => 'auth|csrf', 'uses' => 'Users@destroy'));
 
     // The Users Search.
     Router::post( 'users/search', array('before' => 'auth', 'uses' => 'Users@search'));
 
     // The Roles CRUD.
-    Router::get( 'roles',                array('before' => 'auth',      'uses' => 'Roles@index'));
-    Router::get( 'roles/create',         array('before' => 'auth',      'uses' => 'Roles@create'));
-    Router::post('roles',                array('before' => 'auth|csrf', 'uses' => 'Roles@store'));
-    Router::get( 'roles/(:num)',         array('before' => 'auth',      'uses' => 'Roles@show'));
-    Router::get( 'roles/(:num)/edit',    array('before' => 'auth',      'uses' => 'Roles@edit'));
-    Router::post('roles/(:num)',         array('before' => 'auth|csrf', 'uses' => 'Roles@update'));
-    Router::post('roles/(:num)/destroy', array('before' => 'auth|csrf', 'uses' => 'Roles@destroy'));
+    Router::get( 'roles',              array('before' => 'auth',      'uses' => 'Roles@index'));
+    Router::get( 'roles/create',       array('before' => 'auth',      'uses' => 'Roles@create'));
+    Router::post('roles',              array('before' => 'auth|csrf', 'uses' => 'Roles@store'));
+    Router::get( 'roles/{id}',         array('before' => 'auth',      'uses' => 'Roles@show'));
+    Router::get( 'roles/{id}/edit',    array('before' => 'auth',      'uses' => 'Roles@edit'));
+    Router::post('roles/{id}',         array('before' => 'auth|csrf', 'uses' => 'Roles@update'));
+    Router::post('roles/{id}/destroy', array('before' => 'auth|csrf', 'uses' => 'Roles@destroy'));
 });

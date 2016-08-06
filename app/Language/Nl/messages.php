5fa99a26ba5d51e009373683e14c740375b4597c
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-08-06 10:19:37 +0200

    Update Dutch and Polish Languages

--- app/Language/Nl/messages.php
+++ app/Language/Nl/messages.php
@@ -5,14 +5,16 @@ return array (
   'Database API' => '',
   'Events API' => '',
   'Hello, welcome from the welcome controller and subpage method! <br/>
-This content can be changed in <code>/app/Views/Welcome/SubPage.php</code>' => '',
+This content can be changed in <code>/app/Views/Welcome/SubPage.php</code>' => 'Hallo, welkom van de welcome controller en de subpagina methode! <br/>
+Deze inhoud kan worden veranderd in <code>app/Views/Welcome/SubPage.php</code>',
   'Hello, welcome from the welcome controller! <br/>
-this content can be changed in <code>/app/Views/Welcome/Welcome.php</code>' => '',
-  'Home' => '',
+this content can be changed in <code>/app/Views/Welcome/Welcome.php</code>' => 'Hallo, welkom van de welcome controller! <br/>
+Deze inhoud kan worden veranderd in <code>app/Views/Welcome/Welcome.php</code>',
+  'Home' => 'Home',
   'If you have problems, please paste the above URL into your web browser.' => '',
   'Mailing API' => '',
   'Message sent while pretending. Please, look on <code>{0}</code>' => '',
-  'Open subpage' => '',
+  'Open subpage' => 'Open subpagina',
   'Pagination' => '',
   'Password Reset' => '',
   'Password Sample' => '',
@@ -20,14 +22,14 @@ this content can be changed in <code>/app/Views/Welcome/Welcome.php</code>' => '
   'Please verify your E-mail address' => '',
   'Request API' => '',
   'Session API' => '',
-  'Subpage' => '',
+  'Subpage' => 'Subpagina',
   'Test' => '',
   'Thanks for creating an Account with the {0}. Please follow the link below to verify your email address: {1}' => '',
   'This is a test!!!' => '',
   'Timestamp:' => '',
   'To reset your password, complete this form: {0}.' => '',
   'Validation API' => '',
-  'Welcome' => '',
+  'Welcome' => 'Welkom',
   'Welcome to {0}!' => '',
   'You are not authorized to access this resource.' => '',
 );

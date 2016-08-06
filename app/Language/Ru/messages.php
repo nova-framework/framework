cf2f19e62916ad460f801e1407367025cb481123
XTRO123 <victor.efremov@gmail.com>
2016-08-06 21:28:52 +0300

    update lang_ru

--- app/Language/Ru/messages.php
+++ app/Language/Ru/messages.php
@@ -1,35 +1,35 @@
 <?php
 
 return array (
-  'Cache' => '',
-  'Database API' => '',
-  'Events API' => '',
+  'Cache' => 'Кэш',
+  'Database API' => 'API базы данных',
+  'Events API' => 'API событий',
   'Hello, welcome from the welcome controller and subpage method! <br/>
-This content can be changed in <code>/app/Views/Welcome/SubPage.php</code>' => 'Здраствуй, добро пожаловать в привствующий контролер в метод подстраницы! <br/>
+This content can be changed in <code>/app/Views/Welcome/SubPage.php</code>' => 'Здраствуйте, добро пожаловать в метод подстраницы стартового контролера! <br/>
 Содержимое этого файла можеть быть изменено в <code>app/Views/Welcome/SubPage.php</code>',
   'Hello, welcome from the welcome controller! <br/>
-this content can be changed in <code>/app/Views/Welcome/Welcome.php</code>' => 'Здраствуй, добро пожаловать в привствующий контролер! <br/>
+this content can be changed in <code>/app/Views/Welcome/Welcome.php</code>' => 'Здраствуйте, добро пожаловать в стартовый контролер! <br/>
 Содержимое этого файла может быть изменено в <code>app/Views/Welcome/Welcome.php</code>',
   'Home' => 'Домой',
-  'If you have problems, please paste the above URL into your web browser.' => '',
-  'Mailing API' => '',
-  'Message sent while pretending. Please, look on <code>{0}</code>' => '',
+  'If you have problems, please paste the above URL into your web browser.' => 'Если у вас есть проблемы, пожалуйста, вставьте  URL выше в адресную строку браузера.',
+  'Mailing API' => 'API отправки почты',
+  'Message sent while pretending. Please, look on <code>{0}</code>' => 'Отправлена имитация сообщения. Пожалуйста, обратите внимание на <code>{0}</code>',
   'Open subpage' => 'Открыть подстраницу',
-  'Pagination' => '',
-  'Password Reset' => '',
-  'Password Sample' => '',
-  'Password:' => '',
-  'Please verify your E-mail address' => '',
-  'Request API' => '',
-  'Session API' => '',
+  'Pagination' => 'Открыть подстраницу',
+  'Password Reset' => 'Нумерация страниц',
+  'Password Sample' => 'Сброс пароля',
+  'Password:' => 'Образец пароля',
+  'Please verify your E-mail address' => 'Пожалуйста, подтвердите ваш email адрес',
+  'Request API' => 'API запроса',
+  'Session API' => 'API сессии',
   'Subpage' => 'Подстраница',
-  'Test' => '',
-  'Thanks for creating an Account with the {0}. Please follow the link below to verify your email address: {1}' => '',
-  'This is a test!!!' => '',
-  'Timestamp:' => '',
-  'To reset your password, complete this form: {0}.' => '',
-  'Validation API' => '',
+  'Test' => 'Тест',
+  'Thanks for creating an Account with the {0}. Please follow the link below to verify your email address: {1}' => 'Спасибо за создание учетной записи на {0}. Пожалуйста, перейдите по ссылке ниже, чтобы подтвердить свой адрес электронной почты: {1}',
+  'This is a test!!!' => 'Это тест!!!',
+  'Timestamp:' => 'Метка времени',
+  'To reset your password, complete this form: {0}.' => 'Чтобы сбросить пароль, заполните эту форму: {0}',
+  'Validation API' => 'API проверки',
   'Welcome' => 'Добро пожаловать!',
-  'Welcome to {0}!' => '',
-  'You are not authorized to access this resource.' => '',
+  'Welcome to {0}!' => 'Добро пожаловать на {0}',
+  'You are not authorized to access this resource.' => 'Вы не имеете права доступа к данному ресурсу.',
 );

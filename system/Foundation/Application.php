3d2c3bb85655ba8ccfdc67ffd32c68bfff219a01
David Carr <dave@daveismyname.com>
2016-08-12 21:13:19 +0100

    updated version number

--- system/Foundation/Application.php
+++ system/Foundation/Application.php
@@ -44,7 +44,7 @@ class Application extends Container implements HttpKernelInterface, TerminableIn
      *
      * @var string
      */
-    const VERSION = '3.65.3';
+    const VERSION = '3.65.4';
 
     /**
      * Indicates if the application has "booted".

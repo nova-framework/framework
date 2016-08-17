c22aef550496d534a9e3eaacfd3269016b98231e
DonWagner <michael@soerensens.dk>
2016-08-17 11:35:35 +0200

    Update messages.php

--- app/Modules/Cron/Language/Da/messages.php
+++ app/Modules/Cron/Language/Da/messages.php
@@ -1,10 +1,10 @@
 <?php
 
 return array (
-  'Cron Adapter not found: %s.' => '',
-  'The Adapter <b>{0}<b> is already defined.' => '',
-  'execution failed.' => '',
+  'Cron Adapter not found: %s.' => 'Cron Adapter ikke fundet: %s.',
+  'The Adapter <b>{0}<b> is already defined.' => 'Adapteren <b>{0}</b> er allerede lavet.',
+  'execution failed.' => 'udførelse mislykkedes.',
   'jS M Y H:i:s' => '',
-  'succesfully executed.' => '',
-  '{0} - Cron executed on {1}' => '',
+  'succesfully executed.' => 'udført med succes.',
+  '{0} - Cron executed on {1}' => '{0} - Cron udført den {1}',
 );

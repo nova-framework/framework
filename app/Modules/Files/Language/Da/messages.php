eda6244affa80ca6e346bff83f23448400bc6da4
David Carr <dave@daveismyname.com>
2016-08-18 09:16:45 +0100

    Revert "Danish Language Update"

--- app/Modules/Files/Language/Da/messages.php
+++ app/Modules/Files/Language/Da/messages.php
@@ -1,7 +1,7 @@
 <?php
 
 return array (
-  'Dashboard' => 'Skrivebord',
-  'Files' => 'Filer',
-  'You are not authorized to access this resource.' => 'Du har ikke tilgang til denne side.',
+  'Dashboard' => '',
+  'Files' => '',
+  'You are not authorized to access this resource.' => '',
 );

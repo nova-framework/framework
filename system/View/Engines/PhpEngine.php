e75a8d432caab5b875dc6a29e5de241872ac2f0c
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-08-20 16:42:01 +0200

    Improve View\Engines\PhpEngine

--- system/View/Engines/PhpEngine.php
+++ system/View/Engines/PhpEngine.php
@@ -20,26 +20,36 @@ class PhpEngine implements EngineInterface
     }
 
     /**
+     * Get the evaluated contents of the View.
+     *
+     * @param  string  $path
+     * @param  array   $data
+     * @return string
+     */
+    public function render($path, array $data = array())
+    {
+        return $this->evaluatePath($path, $data);
+    }
+
+    /**
      * Get the evaluated contents of the View at the given path.
      *
      * @param  string  $__path
      * @param  array   $__data
      * @return string
      */
-    public function render($__path, array $__data = array())
+    protected function evaluatePath($__path, $__data)
     {
-        // Start the output buffering.
         ob_start();
 
-        // Extract the data variables.
         extract($__data);
 
-        // We'll evaluate the contents of the View inside a try/catch block so we can
-        // flush out any stray output that might get out before an error occurs or an
-        // exception is thrown. This prevents any partial views from leaking.
+        // We'll evaluate the contents of the view inside a try/catch block so we can
+        // flush out any stray output that might get out before an error occurs or
+        // an exception is thrown. This prevents any partial views from leaking.
         try {
             include $__path;
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             $this->handleViewException($e);
         }
 

76b796e7afe205f4f05ea6f7829847e7b1937dd2
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-08-26 11:26:22 +0200

    Move the global View variables processing from App\Core\Controller to a Event Listener

--- app/Core/Controller.php
+++ app/Core/Controller.php
@@ -26,39 +26,4 @@ class Controller extends BaseController
         parent::__construct();
     }
 
-    protected function before()
-    {
-        // Share on Views the CSRF Token.
-        View::share('csrfToken', Session::token());
-
-        // Calculate the URIs and share them on Views.
-        $uri = Request::path();
-
-        // Prepare the base URI.
-        $baseUri = trim($uri, '/');
-
-        if (! empty($baseUri)) {
-            $parts = explode('/', $baseUri);
-
-            // Make the path equal with the first part if it exists, i.e. 'admin'
-            $baseUri = array_shift($parts);
-
-            // Add to path the next part, if it exists, defaulting to 'dashboard'.
-            if (! empty($parts)) {
-                $baseUri .= '/' .array_shift($parts);
-            } else if ($baseUri == 'admin') {
-                $baseUri .= '/dashboard';
-            }
-        } else {
-            // Respect the URI conventions.
-            $baseUri = '/';
-        }
-
-        View::share('currentUri', $uri);
-        View::share('baseUri', $baseUri);
-
-        // Leave to parent's method the Execution Flow decisions.
-        return parent::before();
-    }
-
 }
--- app/Events.php
+++ app/Events.php
@@ -6,15 +6,40 @@
  * @version 3.0
  */
 
-use Core\View;
-use Helpers\Hooks;
-use Forensics\Console;
-
 
 /** Define Events. */
 
 // Add a Listener Closure to the Event 'framework.controller.executing'.
 Event::listen('router.matched', function($route, $request) {
+    // Calculate the URIs and share them on Views.
+    $uri = $request->path();
+
+    // Prepare the base URI.
+    $segments = $request->segments();
+
+    if (! empty($segments)) {
+        // Make the path equal with the first part if it exists, i.e. 'admin'
+        $baseUri = array_shift($segments);
+
+        // Add to path the next part, if it exists, defaulting to 'dashboard'.
+        if (! empty($segments)) {
+            $baseUri .= '/' .array_shift($segments);
+        } else if ($baseUri == 'admin') {
+            $baseUri .= '/dashboard';
+        }
+    } else {
+        // Respect the URI conventions.
+        $baseUri = '/';
+    }
+
+    View::share('currentUri', $uri);
+    View::share('baseUri', $baseUri);
+
+    // Share on Views the CSRF Token.
+    $session = $request->session();
+
+    View::share('csrfToken', $session->token());
+
     // Run the Hooks associated to the Views.
     $hooks = Hooks::get();
 
@@ -24,9 +49,4 @@ Event::listen('router.matched', function($route, $request) {
         // Share the result into Views.
         View::share($hook, $result);
     }
-});
-
-// Add a Listener Closure to the Event 'nova.framework.booting'.
-Event::listen('nova.framework.booting', function() {
-    Console::logSpeed("Nova Framework booting");
 });

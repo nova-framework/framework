325c62120fa303a333d563316ff5d5b07eeaeb50
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-09-03 13:25:37 +0200

    Improve Tempalte\Template

--- system/Template/Template.php
+++ system/Template/Template.php
@@ -2,17 +2,7 @@
 
 namespace Template;
 
-use View\Engines\EngineInterface;
-use View\Factory as ViewFactory;
 use View\View;
 
 
-class Template extends View
-{
-
-    public function __construct(ViewFactory $factory, EngineInterface $engine, $view, $path, $data = array())
-    {
-        parent::__construct($factory, $engine, $view, $path, $data);
-    }
-
-}
+class Template extends View {}

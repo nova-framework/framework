48b1391e52e014259d2802f5e59081e2d7585b6f
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-09-07 17:17:12 +0200

    Small changes on server.php

--- server.php
+++ server.php
@@ -8,9 +8,12 @@
 
 
 $public_path = 'public/';
+
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
 $uri = urldecode($uri);
-$requested = $public_path.$uri;
+
+$requested = $public_path .$uri;
 
 if (($uri !== '/') && file_exists($requested)) {
     return false;

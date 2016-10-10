f2d9abf514e25e76ec98e416c15239e4aa704af8
David Carr <dave@daveismyname.com>
2016-10-10 17:59:04 +0100

    Update Application.php

--- system/Foundation/Application.php
+++ system/Foundation/Application.php
@@ -36,7 +36,7 @@ class Application extends Container implements HttpKernelInterface, TerminableIn
      *
      * @var string
      */
-    const VERSION = '3.73.1';
+    const VERSION = '3.73.2';
 
     /**
      * Indicates if the application has "booted".

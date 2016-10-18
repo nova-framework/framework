490fba969a6259feb0ea7e85a7a5b30a6889fb97
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-10-18 11:01:20 +0300

    Improve Support\Str

--- system/Support/Str.php
+++ system/Support/Str.php
@@ -421,4 +421,28 @@ class Str
         return static::$studlyCache[$key] = str_replace(' ', '', $value);
     }
 
+    /**
+     * Returns the portion of string specified by the start and length parameters.
+     *
+     * @param  string  $string
+     * @param  int  $start
+     * @param  int|null  $length
+     * @return string
+     */
+    public static function substr($string, $start, $length = null)
+    {
+        return mb_substr($string, $start, $length, 'UTF-8');
+    }
+
+    /**
+     * Make a string's first character uppercase.
+     *
+     * @param  string  $string
+     * @return string
+     */
+    public static function ucfirst($string)
+    {
+        return static::upper(static::substr($string, 0, 1)) .static::substr($string, 1);
+    }
+
 }

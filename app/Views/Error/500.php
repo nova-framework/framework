f64bdd52effa373d277b2e58e8e2f3d1f75c2573
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-12-22 18:52:21 +0200

    Add app/Views/Error/500.php

---
+++ app/Views/Error/500.php
@@ -0,0 +1,14 @@
+<div class="container content">
+    <div class="row">
+        <div class="col-md-12">
+
+            <h1>500</h1>
+
+            <hr />
+
+            <h3><?= __('Internal Server Error'); ?></h3>
+            <p><?= __('Something has gone wrong on the Web Server.'); ?></p>
+
+        </div>
+    </div>
+</div>

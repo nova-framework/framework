f25f232f7767007b684f91394fada96cd6afb1a6
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-12-24 19:58:49 +0200

    Improve the Modules

--- app/Modules/Users/Events.php
+++ app/Modules/Users/Events.php
@@ -10,25 +10,24 @@
 /** Define Events. */
 
 Event::listen('backend.menu', function($user) {
-    if (! $user->hasRole('administrator')) {
-        // No menu items for non-administrators.
-        return array();
+    if ($user->hasRole('administrator')) {
+        $items = array(
+            array(
+                'uri'    => 'admin/users',
+                'title'  => __d('users', 'Users'),
+                'icon'   => 'users',
+                'weight' => 1,
+            ),
+            array(
+                'uri'    => 'admin/roles',
+                'title'  => __d('users', 'Roles'),
+                'icon'   => 'book',
+                'weight' => 2,
+            ),
+        );
+    } else {
+        $items = array();
     }
-
-    $items = array(
-        array(
-            'uri'    => 'admin/users',
-            'title'  => __d('users', 'Users'),
-            'icon'   => 'users',
-            'weight' => 1,
-        ),
-        array(
-            'uri'    => 'admin/roles',
-            'title'  => __d('users', 'Roles'),
-            'icon'   => 'book',
-            'weight' => 2,
-        ),
-    );
 
     return $items;
 });

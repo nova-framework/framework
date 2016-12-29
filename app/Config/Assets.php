a8d2faaae5ae18fe2e425081c37cc6128ded51b0
Daniel Taylor <kingorgg@gmail.com>
2016-12-29 14:18:35 +0000

    Update Comments for Config Files

--- app/Config/Assets.php
+++ app/Config/Assets.php
@@ -7,34 +7,98 @@
  */
 
 return array(
-    // The driver type used for serving the Asset Files.
-    'driver' => 'default',     // Supported: "default" and "custom".
 
-    // The name of Assets Dispatcher used as 'custom' driver.
+    /*
+    |--------------------------------------------------------------------------
+    | Assets Driver
+    |--------------------------------------------------------------------------
+    |
+    | The driver type used for serving the Asset Files.
+    |
+    | Supported: "default" and "custom".
+    |
+    | Default: 'default'
+    |
+    */
+
+    'driver' => 'default',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Assets Driver
+    |--------------------------------------------------------------------------
+    |
+    | The name of Assets Dispatcher used as 'custom' driver.
+    |
+    | Default: 'Shared\Assets\CustomDispatcher'
+    |
+    */
+
     'dispatcher' => 'Shared\Assets\CustomDispatcher',
 
-    // Wheter or not the CSS and JS files are automatically compressed.
+    /*
+    |--------------------------------------------------------------------------
+    | Compress Assets
+    |--------------------------------------------------------------------------
+    |
+    | Whether or not the CSS and JS files are automatically compressed.
+    |
+    | Default: true
+    |
+    */
+
     'compress' => true,
 
-    // The Caching configuration.
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Configuration
+    |--------------------------------------------------------------------------
+    |
+    | The Caching configuration.
+    |
+    */
+
     'cache' => array(
-        // The local Assets Cache options.
+        /*
+        |----------------------------------------------------------------------
+        | Local Assets
+        |----------------------------------------------------------------------
+        |
+        */
+
         'active'   => true,
         'lifeTime' => 1440,
         'baseUri'  => 'cache',
 
-        // The browser Cache Control options.
+        /*
+        |----------------------------------------------------------------------
+        | Browser Cache Control
+        |----------------------------------------------------------------------
+        |
+        */
+
         'ttl'          => 600,
         'maxAge'       => 10800,
         'sharedMaxAge' => 600,
     ),
 
-    // The Valid Vendor Paths - be aware that improper configuration of the Valid Vendor Paths could introduce
-    // severe security issues, try to limit the access to a precise area, where aren't present "unsafe" files.
-    //
-    // '/vendor/almasaeed2010/adminlte/dist/css/AdminLTE.min.css'
-    //          ^____________________^____^____________________This are the parts of path which are validated.
-    //
+    /*
+    |--------------------------------------------------------------------------
+    | Vendor Paths
+    |--------------------------------------------------------------------------
+    |
+    | The Valid Vendor Paths - be aware that improper configuration of the
+    | Valid Vendor Paths could introduce severe security issues, try to limit
+    | the access to a precise area, where there aren't any "unsafe" files
+    | present.
+    |
+    | '/vendor/almasaeed2010/adminlte/dist/css/AdminLTE.min.css'
+    |               ^___________^______^____________________
+    |
+    | These are parts of the path which are validated.
+    |
+    */
+
     'paths' => array(
         'almasaeed2010/adminlte' => array(
             'bootstrap',

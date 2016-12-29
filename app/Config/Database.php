d0cedbd3af10dbdbcbb7fcbdaad838fbbde4eaad
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2016-12-29 14:09:09 +0200

    Adjust the configuration files

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -2,9 +2,8 @@
 /**
  * Database configuration
  *
- * @author David Carr - dave@daveismyname.com
  * @author Virgil-Adrian Teaca - virgil@giulianaeassociati.com
- * @version 3.0
+ * @version 4.0
  */
 
 
--- app/Config/Profiler.php
+++ app/Config/Profiler.php
@@ -3,7 +3,7 @@
  * Profiler
  *
  * @author Virgil-Adrian Teaca - virgil@giulianaeassociati.com
- * @version 3.0
+ * @version 4.0
  */
 
 

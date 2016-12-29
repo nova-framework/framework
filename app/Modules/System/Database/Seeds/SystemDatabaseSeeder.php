0318f60a8e7f60968d5f8cb4f1ed616d48fa57de
Daniel Taylor <kingorgg@gmail.com>
2016-12-29 15:49:23 +0000

    Move Migrations and Seeds for Users

--- app/Modules/System/Database/Seeds/SystemDatabaseSeeder.php
+++ app/Modules/System/Database/Seeds/SystemDatabaseSeeder.php
@@ -18,5 +18,7 @@ class SystemDatabaseSeeder extends Seeder
         Model::unguard();
 
         // $this->call('App\Modules\System\Database\Seeds\FoobarTableSeeder');
+        $this->call('App\Modules\System\Database\Seeds\RolesTableSeeder');
+        $this->call('App\Modules\System\Database\Seeds\UsersTableSeeder');
     }
 }
--- app/Modules/Users/Database/Seeds/UsersDatabaseSeeder.php
+++ app/Modules/Users/Database/Seeds/UsersDatabaseSeeder.php
@@ -18,7 +18,5 @@ class UsersDatabaseSeeder extends Seeder
         Model::unguard();
 
         // $this->call('App\Modules\Users\Database\Seeds\FoobarTableSeeder');
-        $this->call('App\Modules\Users\Database\Seeds\RolesTableSeeder');
-        $this->call('App\Modules\Users\Database\Seeds\UsersTableSeeder');
     }
 }

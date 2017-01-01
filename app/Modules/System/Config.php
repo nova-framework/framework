510c2f427319e3a6d683e712ae619da9ac6d400a
David Carr <dave@daveismyname.com>
2017-01-01 10:46:30 +0000

    Updated comments

--- app/Modules/System/Config.php
+++ app/Modules/System/Config.php
@@ -14,9 +14,17 @@ use Nova\Config\Config;
  */
 
 Config::set('cron', array(
-    /**
-     * The CRON token.
-     * This tool can be used to generate key - http://jeffreybarke.net/tools/codeigniter-encryption-key-generator
-     */
+    /*
+    |--------------------------------------------------------------------------
+    | Encryption Key
+    |--------------------------------------------------------------------------
+    |
+    | This key is used by the encrypter service and should be set
+    | to a random, 32 character string, otherwise these encrypted strings
+    | will not be safe. Please do this before deploying an application!
+    |
+    | This page can be used to generate key - http://novaframework.com/token-generator
+    |
+    */
     'token' => 'SomeRandomStringThere_1234567890',
 ));

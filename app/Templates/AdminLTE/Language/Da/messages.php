b3d12fbe4e9e4ebf5b1240f704ffefcaccd32fb1
DonWagner <michael@soerensens.dk>
2017-01-23 16:32:49 +0100

    Update messages.php

--- app/Templates/AdminLTE/Language/Da/messages.php
+++ app/Templates/AdminLTE/Language/Da/messages.php
@@ -3,11 +3,11 @@
 return array (
   '%d %b %Y, %R' => '',
   'ADMINISTRATION' => '',
-  'Control Panel' => 'Kontorl Panel',
+  'Control Panel' => 'Kontorlpanel',
   'Forgot Password?' => 'Glemt kodeord?',
   'Logout' => 'Log ud',
   'Member since {0}' => 'Medlem siden {0}',
-  'Profile' => 'Bruger konto',
+  'Profile' => 'Brugerkonto',
   'Sign In' => 'Log ind',
   'Sign Up' => 'Tilmeld',
   'Sign out' => 'Log ud',

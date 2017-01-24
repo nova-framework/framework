72ef10056f4eb98c831a3c3a6f5e8b5cef1eb6a3
DonWagner <michael@soerensens.dk>
2017-01-24 14:20:17 +0100

    Update messages.php

--- app/Modules/Dashboard/Language/DA/messages.php
+++ app/Modules/Dashboard/Language/DA/messages.php
@@ -2,6 +2,6 @@
 
 return array (
   'Dashboard' => 'Skrivebord',
-  'Someday, we\'ll have widgets and stuff on here...' => '',
-  'Yup. This is the Dashboard.' => '',
+  'Someday, we\'ll have widgets and stuff on here...' => 'En dag vil der komme noget fedt her på siden..',
+  'Yup. This is the Dashboard.' => 'Jeps.. Dette er skrivebordet',
 );

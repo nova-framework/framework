bc5a4d618b0ab16a89d5c0f12e29d0a1c074d129
DonWagner <michael@soerensens.dk>
2017-01-24 14:24:34 +0100

    Update messages.php

--- app/Language/DA/messages.php
+++ app/Language/DA/messages.php
@@ -4,17 +4,17 @@ return array (
   'Hello, welcome from the welcome controller and subpage method! <br/>
 This content can be changed in <code>/app/Views/Welcome/SubPage.php</code>' => 'Hej. Velkommen fra "Welcome" controlleren og "Subpage" funktionen!<br/>Dette indhold kan ændres i <code>/app/Views/Welcome/SubPage.php</code>.',
   'Hello, welcome from the welcome controller! <br/>
-this content can be changed in <code>/app/Views/Welcome/Welcome.php</code>' => '',
+this content can be changed in <code>/app/Views/Welcome/Welcome.php</code>' => 'Hej. Velkommen fra "Welcome" controlleren!<br/>Dette indhold kan ændres i <code>/app/Views/Welcome/Welcome.php</code>.',
   'Home' => 'Hjem',
   'If you have problems, please paste the above URL into your web browser.' => 'Hvis du har problemer skal du indsætte den ovenstående URL ind i din webbrowser.',
-  'Internal Server Error' => '',
+  'Internal Server Error' => 'Intern server fejl',
   'Open subpage' => 'Åben underside',
   'Password Reset' => 'Nulstil kodeord',
   'Please verify your E-mail address' => 'Bekræft venligst din mailadresse',
-  'Something has gone wrong on the Web Server.' => '',
+  'Something has gone wrong on the Web Server.' => 'Der er gået noget galt på web-serveren',
   'Subpage' => 'Underside',
   'Thanks for creating an Account with the {0}. Please follow the link below to verify your email address: {1}' => 'Tak fordi du har oprettet en bruger på {0}. Brug venligst linket her under til at bekræfte din mailadresse: <a href="{1}">{1}</a>',
   'To reset your password, complete this form: {0}.' => 'For at nulstille dit kodeord, skal du udfyld denne formular: {0}.',
   'Welcome' => 'Velkommen',
-  'You are not authorized to access this resource.' => '',
+  'You are not authorized to access this resource.' => 'Du er ikke godkendt til at tilgå denne ressource',
 );

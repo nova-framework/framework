44cc33dcc56c9602f927a65fce90b5c6c02c5f36
DonWagner <michael@soerensens.dk>
2017-02-02 12:50:32 +0100

    Update messages.php

--- modules/Users/Language/FA/messages.php
+++ modules/Users/Language/FA/messages.php
@@ -1,7 +1,7 @@
 <?php
 
 return array (
-  '%d %b %Y, %R' => '',
+  '%d %b %Y, %H:%M' => '',
   '<< Previous Page' => '<< صفحه قبلی',
   '<b>{0}</b>, you have successfully logged in.' => '',
   'Activate your Account!' => 'حساب خود را فعال کنید!',

9a1213096dad4d96773f9169ce1f5f197cbc8b9b
Victor Efremov <victor.efremov@gmail.com>
2017-02-03 00:24:56 +0300

    small fix Lang

--- app/Config/Languages.php
+++ app/Config/Languages.php
@@ -9,18 +9,18 @@
 
 
 return array(
-    'cs' => array('info' => 'Czech',     'name' => 'čeština',    'locale' => 'cs_CZ', 'dir' => 'ltr'),
+    'cs' => array('info' => 'Czech',     'name' => 'Ćeština',    'locale' => 'cs_CZ', 'dir' => 'ltr'),
     'da' => array('info' => 'Danish',    'name' => 'Dansk',      'locale' => 'da_DK', 'dir' => 'ltr'),
     'de' => array('info' => 'German',    'name' => 'Deutsch',    'locale' => 'de_DE', 'dir' => 'ltr'),
     'en' => array('info' => 'English',   'name' => 'English',    'locale' => 'en_US', 'dir' => 'ltr'),
     'es' => array('info' => 'Spanish',   'name' => 'Español',    'locale' => 'es_ES', 'dir' => 'ltr'),
     'fa' => array('info' => 'Persian',   'name' => 'پارسی',      'locale' => 'fa_IR', 'dir' => 'rtl'),
     'fr' => array('info' => 'French',    'name' => 'Français',   'locale' => 'fr_FR', 'dir' => 'ltr'),
-    'hu' => array('info' => 'Hungarian', 'name' => 'magyar',     'locale' => 'hu_HU', 'dir' => 'ltr'),
-    'it' => array('info' => 'Italian',   'name' => 'italiano',   'locale' => 'it_IT', 'dir' => 'ltr'),
+    'hu' => array('info' => 'Hungarian', 'name' => 'Magyar',     'locale' => 'hu_HU', 'dir' => 'ltr'),
+    'it' => array('info' => 'Italian',   'name' => 'Italiano',   'locale' => 'it_IT', 'dir' => 'ltr'),
     'ja' => array('info' => 'Japanesse', 'name' => '日本語',      'locale' => 'ja_JA', 'dir' => 'ltr'),
     'nl' => array('info' => 'Dutch',     'name' => 'Nederlands', 'locale' => 'nl_NL', 'dir' => 'ltr'),
-    'pl' => array('info' => 'Polish',    'name' => 'polski',     'locale' => 'pl_PL', 'dir' => 'ltr'),
+    'pl' => array('info' => 'Polish',    'name' => 'Polski',     'locale' => 'pl_PL', 'dir' => 'ltr'),
     'ro' => array('info' => 'Romanian',  'name' => 'Română',     'locale' => 'ro_RO', 'dir' => 'ltr'),
-    'ru' => array('info' => 'Russian',   'name' => 'ру́сский',    'locale' => 'ru_RU', 'dir' => 'ltr'),
+    'ru' => array('info' => 'Russian',   'name' => 'Русский',    'locale' => 'ru_RU', 'dir' => 'ltr'),
 );

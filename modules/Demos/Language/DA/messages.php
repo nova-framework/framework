2f4db8f1f8fceb6033502aec539e594a7e061fe3
Michael <michael@soerensens.dk>
2017-02-08 14:50:17 +0100

    Danish Language Update

--- modules/Demos/Language/DA/messages.php
+++ modules/Demos/Language/DA/messages.php
@@ -1,13 +1,13 @@
 <?php
 
 return array (
-  'Bounce Rate' => '',
+  'Bounce Rate' => 'Afvisningsprocent',
   'Cache' => '',
   'Database API' => '',
   'Events API' => '',
   'Mailing API' => '',
   'Message sent while pretending. Please, look on <code>{0}</code>' => 'Beskeden er sendt for sjovt. Kig venligst på <code>{0}</code>',
-  'New Orders' => '',
+  'New Orders' => 'Nye ordrer',
   'Pagination' => 'Sideinddeling',
   'Password Sample' => 'Kodeords eksempel',
   'Password:' => 'Kodeord',
@@ -17,7 +17,7 @@ return array (
   'The catch-all Route' => 'Catch-alt routeren',
   'This is a test!!!' => 'Dette er en test',
   'Timestamp:' => 'Tidsstemple',
-  'Unique Visitors' => '',
+  'Unique Visitors' => 'Unikke besøgende',
   'Validation API' => '',
   'Welcome to {0}!' => 'Velkommen til {0}',
 );

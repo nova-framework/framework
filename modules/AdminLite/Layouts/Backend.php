b8c6817f5814c68f58db04e3447a91e421608882
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-02-08 10:46:02 +0200

    Split the Backend module into AdminLite (theme) and Dashboard

---
+++ modules/AdminLite/Layouts/Backend.php
@@ -0,0 +1,264 @@
+<?php
+/**
+ * Backend Default Layout
+ */
+
+// Prepare the current User Info.
+$user = Auth::user();
+
+// Generate the Language Changer menu.
+$langCode = Language::code();
+$langName = Language::name();
+
+$languages = Config::get('languages');
+
+//
+ob_start();
+
+foreach ($languages as $code => $info) {
+?>
+<li class="header <?php if ($code == $langCode) { echo 'active'; } ?>">
+    <a href='<?= site_url('language/' .$code); ?>' title='<?= $info['info']; ?>'><?= $info['name']; ?></a>
+</li>
+<?php
+}
+
+$langMenuLinks = ob_get_clean();
+?>
+<!DOCTYPE html>
+<html lang="<?= $langCode; ?>">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title><?= $title; ?> | <?= Config::get('app.name', SITE_TITLE); ?></title>
+    <?= isset($meta) ? $meta : ''; // Place to pass data / plugable hook zone ?>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <?php
+    Assets::css(array(
+        // Bootstrap 3.3.5
+        vendor_url('bootstrap/css/bootstrap.min.css', 'almasaeed2010/adminlte'),
+        // Font Awesome
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css',
+        // Ionicons
+        'https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css',
+        // Select2
+        vendor_url('plugins/select2/select2.min.css', 'almasaeed2010/adminlte'),
+        // Theme style
+        vendor_url('dist/css/AdminLTE.min.css', 'almasaeed2010/adminlte'),
+        // AdminLTE Skins
+        vendor_url('dist/css/skins/_all-skins.min.css', 'almasaeed2010/adminlte'),
+
+        // Custom CSS
+        resource_url('css/style.css', 'Backend'),
+    ));
+
+    echo isset($css) ? $css : ''; // Place to pass data / plugable hook zone
+?>
+
+<style>
+.pagination {
+    margin: 0;
+}
+
+.pagination > li > a, .pagination > li > span {
+  padding: 5px 10px;
+}
+</style>
+
+<?php
+    //Add Controller specific JS files.
+    Assets::js(array(
+        vendor_url('plugins/jQuery/jquery-2.2.3.min.js', 'almasaeed2010/adminlte'),
+    ));
+
+    ?>
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body class="hold-transition skin-<?= Config::get('app.color_scheme', 'blue'); ?> sidebar-mini">
+<div class="wrapper">
+
+  <!-- Main Header -->
+  <header class="main-header">
+
+    <!-- Logo -->
+    <a href="<?= site_url('admin/dashboard'); ?>" class="logo">
+      <!-- mini logo for sidebar mini 50x50 pixels -->
+      <span class="logo-mini">CP</span>
+      <!-- logo for regular state and mobile devices -->
+      <span class="logo-lg"><?= __d('admin_lite', 'Control Panel'); ?></span>
+    </a>
+
+    <!-- Header Navbar -->
+    <nav class="navbar navbar-static-top" role="navigation">
+      <!-- Sidebar toggle button-->
+      <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+        <span class="sr-only">Toggle navigation</span>
+      </a>
+      <!-- Navbar Right Menu -->
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav" style="margin-right: 10px;">
+          <li <?php if($baseUri == 'admin/messages') echo 'class="active"'; ?>>
+            <a href="<?= site_url('admin/messages'); ?>">
+              <i class='fa fa-envelope'></i>  <span class="hidden-xs"><?= __d('admin_lite', 'Messages'); ?></span>
+              <?php if (isset($privateMessageCount) && ($privateMessageCount > 0)) echo '<span class="label label-success">' .$privateMessageCount .'</span>'; ?>
+            </a>
+          </li>
+          <li <?php if($baseUri == 'admin/notifications') echo 'class="active"'; ?>>
+            <a href="<?= site_url('admin/notifications'); ?>" title="<?= __d('admin_lite', 'Notifications'); ?>">
+              <i class='fa fa-bell'></i></span>  <span class="hidden-xs"><?= __d('admin_lite', 'Notifications'); ?></span>
+              <?php if (isset($notificationCount) && ($notificationCount > 0)) echo '<span class="label label-danger">' .$notificationCount .'</span>'; ?>
+            </a>
+          </li>
+          <li class="dropdown language-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <i class='fa fa-language'></i> <?= $langName; ?>
+            </a>
+            <ul class="dropdown-menu">
+              <?= $langMenuLinks; ?>
+            </ul>
+          </li>
+          <!-- User Account Menu -->
+          <li class="dropdown user user-menu">
+            <!-- Menu Toggle Button -->
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <!-- The user image in the navbar-->
+              <img src="<?= $user->present()->picture(); ?>" class="user-image" alt="User Image">
+              <!-- hidden-xs hides the username on small devices so only the image appears. -->
+              <span class="hidden-xs"><?= $user->username; ?></span>
+            </a>
+            <ul class="dropdown-menu">
+              <!-- The user image in the menu -->
+              <li class="user-header">
+                <img src="<?= $user->present()->picture(); ?>" class="img-circle" alt="User Image">
+
+                <p>
+                  <?= $user->present()->name(); ?> - <?= $user->role->name; ?>
+                  <small><?= __d('admin_lite', 'Member since {0}', $user->present()->memberSince()); ?></small>
+                </p>
+              </li>
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-left">
+                  <a href="<?= site_url('admin/profile'); ?>" class="btn btn-default btn-flat"><?= __d('admin_lite', 'Profile'); ?></a>
+                </div>
+                <div class="pull-right">
+                  <a href="<?= site_url('logout'); ?>" class="btn btn-default btn-flat"><?= __d('admin_lite', 'Sign out'); ?></a>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+  <!-- Left side column. contains the logo and sidebar -->
+  <aside class="main-sidebar">
+
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+        <!-- search form -->
+        <form action="<?= site_url('admin/users/search'); ?>" method="POST" class="sidebar-form">
+            <div class="input-group">
+                <input type="text" name="query" class="form-control" placeholder="Search...">
+                <span class="input-group-btn">
+                    <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
+                    </button>
+                </span>
+            </div>
+        </form>
+        <!-- Sidebar Menu -->
+        <ul class="sidebar-menu">
+            <li class="header"><?= __d('admin_lite', 'ADMINISTRATION'); ?></li>
+            <?php foreach ($menuItems as $item) { ?>
+            <?php $children = array_get($item, 'children', array()); ?>
+            <?php if (! empty($children)) { ?>
+            <?php $paths = array_pluck($children, 'uri'); ?>
+            <li class="treeview<?php if (in_array($currentUri, $paths)) { echo " active"; } ?>">
+                <a href="#">
+                    <i class="fa fa-<?= $item['icon'] ?>"></i> <span class="hidden-xs"><?= $item['title']; ?></span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <?php foreach ($children as $child) { ?>
+                    <li <?php if ($currentUri == $child['uri']) { echo "class='active'"; } ?>>
+                        <a href="<?= site_url($child['uri']); ?>"><i class="fa fa-circle-o"></i> <span><?= $child['title']; ?> </span> <?= $child['label']; ?></a>
+                    </li>
+                    <?php } ?>
+                </ul>
+            </li>
+            <?php } else { ?>
+            <li <?php if ($baseUri == $item['uri']) { echo "class='active'"; } ?>>
+                <a href="<?= site_url($item['uri']); ?>"><i class="fa fa-<?= $item['icon'] ?>"></i> <span class="hidden-xs"><?= $item['title']; ?> </span> <?= $item['label']; ?></a>
+            </li>
+            <?php } ?>
+            <?php } ?>
+        </ul>
+        <!-- /.sidebar-menu -->
+    </section>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <?= $content; ?>
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      <?php if(Config::get('app.debug')) { ?>
+      <small><!-- DO NOT DELETE! - Profiler --></small>
+      <?php } ?>
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; <?php echo date('Y'); ?> <a href="http://www.novaframework.com/" target="_blank"><b>Nova Framework <?= $version; ?> / Kernel <?= VERSION; ?></b></a> - </strong> All rights reserved.
+  </footer>
+
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED JS SCRIPTS -->
+<?php
+Assets::js(array(
+    // Bootstrap 3.3.5
+    vendor_url('bootstrap/js/bootstrap.min.js', 'almasaeed2010/adminlte'),
+    // AdminLTE App
+    vendor_url('dist/js/app.min.js', 'almasaeed2010/adminlte'),
+    // Select2
+    vendor_url('plugins/select2/select2.full.min.js', 'almasaeed2010/adminlte')
+));
+
+echo isset($js) ? $js : ''; // Place to pass data / plugable hook zone
+
+echo isset($footer) ? $footer : ''; // Place to pass data / plugable hook zone
+?>
+
+<script>
+$(function () {
+    //Initialize Select2 Elements
+    $(".select2").select2();
+});
+</script>
+
+<!-- Optionally, you can add Slimscroll and FastClick plugins.
+     Both of these plugins are recommended to enhance the
+     user experience. Slimscroll is required when using the
+     fixed layout. -->
+
+<!-- DO NOT DELETE! - Forensics Profiler -->
+
+</body>
+</html>
---
+++ modules/AdminLite/Providers/ModuleServiceProvider.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Modules\AdminLite\Providers;
+
+use Nova\Module\Support\Providers\ModuleServiceProvider as ServiceProvider;
+
+
+class ModuleServiceProvider extends ServiceProvider
+{
+    /**
+     * The additional provider class names.
+     *
+     * @var array
+     */
+    protected $providers = array();
+
+
+    /**
+     * Bootstrap the Application Events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        parent::boot();
+
+        //
+        $path = realpath(__DIR__ .'/../');
+
+        // Configure the Package.
+        $this->package('Modules/AdminLite', 'admin_lite', $path);
+
+        // Bootstrap the Package.
+        $path = $path .DS .'Bootstrap.php';
+
+        $this->bootstrapFrom($path);
+    }
+
+    /**
+     * Register the AdminLite module Service Provider.
+     *
+     * This service provider is a convenient place to register your modules
+     * services in the IoC container. If you wish, you may make additional
+     * methods or service providers to keep the code more focused and granular.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        parent::register();
+
+        //
+    }
+
+}

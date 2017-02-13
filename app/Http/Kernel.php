d21bebb9806b45af480022c26bda3302838ef608
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-02-13 20:05:50 +0200

    Improve the Middleware

---
+++ app/Http/Kernel.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http;
+
+use Nova\Foundation\Http\Kernel as HttpKernel;
+
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The Application's global HTTP Middleware stack.
+     *
+     * @var array
+     */
+    protected $middleware = array(
+        'Nova\Foundation\Http\Middleware\CheckForMaintenanceMode',
+        'App\Http\Middleware\EncryptCookies',
+        'Nova\Cookie\Middleware\AddQueuedCookiesToResponse',
+        'Nova\Session\Middleware\StartSession',
+        'Nova\Foundation\Http\Middleware\ServeAssetFile',
+        'Nova\View\Middleware\ShareErrorsFromSession',
+        'App\Http\Middleware\VerifyCsrfToken',
+        'Nova\Foundation\Http\Middleware\SetupLanguage',
+        'App\Http\Middleware\MinifyContent',
+    );
+
+    /**
+     * The Application's route Middleware.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = array(
+        'auth'       => 'App\Http\Middleware\Authenticate',
+        'auth.basic' => 'Nova\Auth\Middleware\AuthenticateWithBasicAuth',
+        'guest'      => 'App\Http\Middleware\RedirectIfAuthenticated',
+        'referer'    => 'App\Http\Middleware\CheckForReferer',
+    );
+}

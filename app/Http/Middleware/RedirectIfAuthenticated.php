5de54e7a286c624a694226019369d6fba520dba8
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-03-20 01:09:44 +0200

    Implementation of the Multi-Auth support

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Middleware;
 
-use Nova\Foundation\Application;
+use Nova\Support\Facades\Auth;
 use Nova\Support\Facades\Redirect;
 
 use Closure;
@@ -33,11 +33,12 @@ class RedirectIfAuthenticated
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
+     * @param  string|null  $guard
      * @return mixed
      */
-    public function handle($request, Closure $next)
+    public function handle($request, Closure $next, $guard = null)
     {
-        if ($this->auth->check()) {
+        if (Auth::guard($guard)->check()) {
             return Redirect::to('admin/dashboard');
         }
 

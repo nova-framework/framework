72518d803aacc89800a140c85e8e2be1d24a3861
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-03-20 23:16:34 +0200

    Adjust App\Http\Middleware\RedirectIfAuthenticated

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -10,23 +10,6 @@ use Closure;
 
 class RedirectIfAuthenticated
 {
-    /**
-     * The Guard implementation.
-     *
-     * @var Guard
-     */
-    protected $auth;
-
-    /**
-     * Create a new filter instance.
-     *
-     * @param  Guard  $auth
-     * @return void
-     */
-    public function __construct(Application $app)
-    {
-        $this->auth = $app['auth'];
-    }
 
     /**
      * Handle an incoming request.

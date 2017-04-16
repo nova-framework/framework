a2ecbc70739641eb51a74b827f7d4251fc89aac0
Victor Efremov <victor.efremov@gmail.com>
2017-04-17 02:01:58 +0300

    Correction of typos

--- modules/Users/Views/Authorize/Login.php
+++ modules/Users/Views/Authorize/Login.php
@@ -57,7 +57,7 @@
 <script type="text/javascript">
 
 var captchaCallback = function() {
-    grecaptcha.render('captcha', {'sitekey' : '<?= Config::get("reCaptcha.siteKey"); ?>'});
+    recaptcha.render('captcha', {'sitekey' : '<?= Config::get("reCaptcha.siteKey"); ?>'});
 };
 
 </script>

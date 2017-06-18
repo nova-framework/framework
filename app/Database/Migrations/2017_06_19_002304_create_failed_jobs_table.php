f957a2943d96ab6471dbe2ecc79bec815c2406fd
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-06-19 02:36:03 +0300

    Overall improvements

---
+++ app/Database/Migrations/2017_06_19_002304_create_failed_jobs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Nova\Database\Schema\Blueprint;
+use Nova\Database\Migrations\Migration;
+
+
+class CreateFailedJobsTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('failed_jobs', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->text('connection');
+            $table->text('queue');
+            $table->text('payload');
+            $table->timestamp('failed_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('failed_jobs');
+    }
+
+}

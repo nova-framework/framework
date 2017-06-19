bd9ec5e1abce8a8d6e07eb13667327e48e378300
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-06-19 21:52:00 +0300

    Improve the NestedSet plugin

--- plugins/NestedSet/src/Providers/PluginServiceProvider.php
+++ plugins/NestedSet/src/Providers/PluginServiceProvider.php
@@ -8,29 +8,29 @@ use Nova\Support\ServiceProvider;
 class PluginServiceProvider extends ServiceProvider
 {
 
-    /**
-     * Bootstrap the Application Events.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        $path = realpath(__DIR__ .'/../');
+	/**
+	 * Bootstrap the Application Events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$path = realpath(__DIR__ .'/../');
 
-        // Configure the Package.
-        $this->package('NestedSet', 'nested_set', $path);
+		// Configure the Package.
+		$this->package('NestedSet', 'nested_set', $path);
 
-        //
-    }
+		//
+	}
 
-    /**
-     * Register the NestedSet plugin Service Provider.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	/**
+	 * Register the NestedSet plugin Service Provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
 
 }

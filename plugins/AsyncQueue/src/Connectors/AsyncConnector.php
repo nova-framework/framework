f9a3f53096a697aa0bbc5e673147b6d0ac8feb6c
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-06-19 14:14:48 +0300

    Initial implementation of the AsyncQueue plugin

---
+++ plugins/AsyncQueue/src/Connectors/AsyncConnector.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AsyncQueue\Connectors;
+
+use Nova\Queue\Connectors\DatabaseConnector;
+use Nova\Support\Arr;
+
+use AsyncQueue\Queues\AsyncQueue;
+
+
+class AsyncConnector extends DatabaseConnector
+{
+
+	/**
+	 * Establish a queue connection.
+	 *
+	 * @param array $config
+	 *
+	 * @return \Nova\Queue\Contracts\QueueInterface
+	 */
+	public function connect(array $config)
+	{
+		$connection = Arr::get($config, 'connection');
+
+		return new AsyncQueue(
+			$this->connections->connection($connection),
+
+			$config['table'],
+			$config['queue'],
+
+			Arr::get($config, 'expire', 60),
+			Arr::get($config, 'binary', 'php'),
+			Arr::get($config, 'binaryArgs', ''),
+			Arr::get($config, 'connectionName', '')
+		);
+	}
+}

966a3a73de51b128362d136e734584490a0e24ec
amiroperator <contact@amiroperator.ir>
2017-08-01 16:50:04 +0430

    Update Persian language

--- plugins/Backup/src/Language/FA/messages.php
+++ plugins/Backup/src/Language/FA/messages.php
@@ -1,12 +1,12 @@
 <?php
 
 return array (
-  'Database backup failed. {0}' => '',
-  'Database backup was successful. Saved to {0}' => '',
-  'Database backup was successful. {0} was saved in the dumps folder.' => '',
-  'Database restore failed.' => '',
-  'Please select one of the following dumps:' => '',
-  'Uncompress of gzipped file failed.' => '',
-  'You haven\'t saved any dumps.' => '',
-  '{0} was successfully restored.' => '',
+  'Database backup failed. {0}' => 'پشیبان گیری از پایگاه داده شکست خورد. {0}',
+  'Database backup was successful. Saved to {0}' => 'پشیبان از پایگاه داده با موفقیت انجام شد. دخیره شد در: {0}',
+  'Database backup was successful. {0} was saved in the dumps folder.' => 'پشتیبان از پایگاه داده با موفقیت انجام شد. {0} أر پوشه dumps ذخیره شد.',
+  'Database restore failed.' => 'بازیابی دیابیس شکست خورد',
+  'Please select one of the following dumps:' => 'لطفا یکی از dump های زیر را انتخاب کنید:',
+  'Uncompress of gzipped file failed.' => 'استخراج فایل gzip شکست خورد.',
+  'You haven\'t saved any dumps.' => 'شما هیچ dump ذخیره شده ای ندارید.',
+  '{0} was successfully restored.' => '{0} با موفقیت بازیابی شد.',
 );
--- plugins/Reminders/src/Language/FA/messages.php
+++ plugins/Reminders/src/Language/FA/messages.php
@@ -1,12 +1,12 @@
 <?php
 
 return array (
-  'If you did not request a password reset, no further action is required.' => '',
-  'Password has been reset!' => '',
-  'Password reminder sent!' => '',
-  'Passwords must be at least six characters and match the confirmation.' => '',
-  'Reset Password' => '',
-  'This password reset token is invalid.' => '',
-  'We can\'t find a user with that e-mail address.' => '',
-  'You are receiving this email because we received a password reset request for your account.' => '',
+  'If you did not request a password reset, no further action is required.' => 'اگر شما درخواست بازنشانی رمزعبور را ندادید، نیازی به انجام کاری نیست.',
+  'Password has been reset!' => 'رمزعبور شما بازنشانی شد!',
+  'Password reminder sent!' => 'یادآور رمزعبور ارسال شد!',
+  'Passwords must be at least six characters and match the confirmation.' => 'رمزعبور باید حداقل شش کاراکتر و مطابق با تکرار آن باشد.',
+  'Reset Password' => 'بازنشانی رمزعبور',
+  'This password reset token is invalid.' => 'کلید بازنشانی رمزعبور نامعتبر است.',
+  'We can\'t find a user with that e-mail address.' => 'با نتوانستیم کاربری با آن ایمیل پیدا نماییم.',
+  'You are receiving this email because we received a password reset request for your account.' => 'شما این ایمیل را دریافت نمودید زیرا ما یک درخواست بازنشانی رمزعبور از حساب شما دریافت نمودیم.',
 );

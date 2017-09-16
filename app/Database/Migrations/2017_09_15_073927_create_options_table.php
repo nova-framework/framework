a7dd07cc89e137d0363defa46c66e458883dd31d
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-09-16 19:52:51 +0300

    Update the app/Database/Migrations/2017_09_15_073927_create_options_table.php

--- app/Database/Migrations/2017_09_15_073927_create_options_table.php
+++ app/Database/Migrations/2017_09_15_073927_create_options_table.php
@@ -16,7 +16,6 @@ class CreateOptionsTable extends Migration
         Schema::create('options', function (Blueprint $table)
         {
             $table->increments('id');
-            $table->string('namespace', 100)->nullable();
             $table->string('group', 100);
             $table->string('item', 255)->nullable();
             $table->text('value')->nullable();

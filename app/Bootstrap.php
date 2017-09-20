2829656504cee03029c441b89d505f0491051162
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-09-21 02:03:07 +0300

    Remove View/Layout test

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -6,7 +6,7 @@ use Nova\Http\Request;
 /**
  * Listener Closure to the Event 'router.matched'.
  */
-Event::listen('router.matched', function($route, Request $request)
+Event::listen('router.matched', function ($route, Request $request)
 {
     // Share the Views the current URI.
     View::share('currentUri', $request->path());

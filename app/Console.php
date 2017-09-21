cfe2706fb0ed9f032d7a092aa2b8602f2605e8ad
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-09-21 22:40:45 +0300

    Small change

--- app/Console.php
+++ app/Console.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Console - Register the Forge Commands and Schedule
+ * Console - register the Forge Commands and Schedule
  *
  * @author Virgil-Adrian Teaca - virgil@giulianaeassociati.com
  * @version 3.0

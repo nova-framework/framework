5dc17642e71716442fad3440ba86e6bf169d941c
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-09-22 23:09:14 +0300

    Overall improvements

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,10 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        //
+         // Schedule the flushing of expired password reminders.
+        $schedule->command('auth:clear-reminders')->daily();
+
+        // Schedule the Database Backup.
+        $schedule->command('db:backup')->dailyAt('4:30');
     }
 }

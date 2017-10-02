e845d4c317884635b8eac7742af5297df8590040
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-02 15:05:16 +0300

    Overall improvements

--- webroot/index.php
+++ webroot/index.php
@@ -22,13 +22,13 @@ define('APPPATH', BASEPATH .'app' .DS);
 // Load the Composer Autoloader
 //--------------------------------------------------------------------------
 
-require BASEPATH .str_replace('/', DS, 'vendor/autoload.php');
+require BASEPATH .'vendor' .DS .'autoload.php';
 
 //--------------------------------------------------------------------------
 // Bootstrap the Framework and get the Application instance
 //--------------------------------------------------------------------------
 
-$app = require_once APPPATH .str_replace('/', DS, 'Foundation/Boot/Start.php');
+$app = require_once APPPATH .'Foundation' .DS .'App.php';
 
 //--------------------------------------------------------------------------
 // Run the Application

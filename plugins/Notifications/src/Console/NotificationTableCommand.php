02e8c03f7ca672af922a6b8b5479b49f9270fdeb
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-04 20:30:02 +0300

    Update the Plugins

--- plugins/Notifications/src/Console/NotificationTableCommand.php
+++ plugins/Notifications/src/Console/NotificationTableCommand.php
@@ -53,7 +53,7 @@ class NotificationTableCommand extends Command
      *
      * @return void
      */
-    public function fire()
+    public function handle()
     {
         $fullPath = $this->createBaseMigration();
 

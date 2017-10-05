267283667b88178dc54f251df0100702f2996f29
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-05 04:29:00 +0300

    Improve the Users module

--- app/Modules/Users/Events.php
+++ app/Modules/Users/Events.php
@@ -6,6 +6,8 @@
  * @version 3.0
  */
 
+use App\Models\User;
+
 
 /** Define Events. */
 
@@ -13,7 +15,7 @@ Event::listen('backend.menu', function ($user)
 {
     $items = array();
 
-    if ($user->can('list', 'App\Models\User')) {
+    if ($user->can('list', User::class)) {
         $items[] = array(
             'uri'    => 'admin/users',
             'title'  => __d('users', 'Users'),

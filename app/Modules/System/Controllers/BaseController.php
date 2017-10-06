cf3acf93f4692b599b976c4ca4d082fad597a9ca
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-06 08:01:45 +0300

    Improve the App\Modules\System\Controllers\BaseController

--- app/Modules/System/Controllers/BaseController.php
+++ app/Modules/System/Controllers/BaseController.php
@@ -8,6 +8,7 @@
 
 namespace App\Modules\System\Controllers;
 
+use Nova\Database\ORM\Builder as ModelBuilder;
 use Nova\Support\Facades\Auth;
 use Nova\Support\Facades\Event;
 use Nova\Support\Facades\Redirect;
@@ -134,5 +135,147 @@ abstract class BaseController extends Controller
         });
 
         return $items;
+    }
+
+    /**
+     * Server Side Processor for DataTables.
+     *
+     * @param Nova\Database\Query\Builder|Nova\Database\ORM\Builder $query
+     * @param array $input
+     * @param array $options
+     *
+     * @return array
+     */
+    protected function dataTable($query, array $input, array $options)
+    {
+        $columns = Arr::get($input, 'columns', array());
+
+        // Compute the total count.
+        $recordsTotal = $query->count();
+
+        // Compute the draw.
+        $draw = intval(Arr::get($input, 'draw', 0));
+
+        // Handle the global searching.
+        $search = trim(Arr::get($input, 'search.value'));
+
+        if (! empty($search)) {
+            $query->whereNested(function($query) use($columns, $options, $search)
+            {
+                foreach($columns as $column) {
+                    $data = $column['data'];
+
+                    $option = Arr::first($options, function ($key, $value) use ($data)
+                    {
+                        return ($value['data'] == $data);
+                    });
+
+                    if ($column['searchable'] == 'true') {
+                        $query->orWhere($option['field'], 'LIKE', '%' .$search .'%');
+                    }
+                }
+            });
+        }
+
+        // Handle the column searching.
+        foreach($columns as $column) {
+            $data = $column['data'];
+
+            $option = Arr::first($options, function ($key, $value) use ($data)
+            {
+                return ($value['data'] == $data);
+            });
+
+            $search = trim(Arr::get($column, 'search.value'));
+
+            if (($column['searchable'] == 'true') && (strlen($search) > 0)) {
+                $query->where($option['field'], 'LIKE', '%' .$search .'%');
+            }
+        }
+
+        // Compute the filtered count.
+        $recordsFiltered = $query->count();
+
+        // Handle the column ordering.
+        $orders = Arr::get($input, 'order', array());
+
+        foreach ($orders as $order) {
+            $index = intval($order['column']);
+
+            $column = Arr::get($input, 'columns.' .$index, array());
+
+            //
+            $data = $column['data'];
+
+            $option = Arr::first($options, function ($key, $value) use ($data)
+            {
+                return ($value['data'] == $data);
+            });
+
+            if ($column['orderable'] == 'true') {
+                $dir = ($order['dir'] === 'asc') ? 'ASC' : 'DESC';
+
+                $field = $option['field'];
+
+                if ($query instanceof ModelBuilder) {
+                    $model = $query->getModel();
+
+                    $field = $model->getTable() .'.' .$field;
+                }
+
+                $query->orderBy($field, $dir);
+            }
+        }
+
+        // Handle the pagination.
+        $start  = Arr::get($input, 'start',  0);
+        $length = Arr::get($input, 'length', 25);
+
+        $query->skip($start)->take($length);
+
+        // Retrieve the data from database.
+        $results = $query->get();
+
+        //
+        // Format the data on respect of DataTables specs.
+
+        $columns = array();
+
+        foreach ($options as $option) {
+            $key = $option['data'];
+
+            //
+            $field = Arr::get($option, 'field');
+
+            $columns[$key] = Arr::get($option, 'uses', $field);
+        }
+
+        //
+        $data = array();
+
+        foreach ($results as $result) {
+            $record = array();
+
+            foreach ($columns as $key => $value) {
+                // Process for standard columns.
+                if (is_string($value)) {
+                    $record[$key] = $result->{$value};
+
+                    continue;
+                }
+
+                // Process for dynamic columns.
+                $record[$key] = call_user_func($value, $result, $key);
+            }
+
+            $data[] = $record;
+        }
+
+        return array(
+            "draw"            => $draw,
+            "recordsTotal"    => $recordsTotal,
+            "recordsFiltered" => $recordsFiltered,
+            "data"            => $data
+        );
     }
 }

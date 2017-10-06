0aa74396a362d2ed69e9d47f6b26cc0cffddebe0
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-06 06:43:58 +0300

    Small change

--- app/Themes/AdminLite/Layouts/Backend.php
+++ app/Themes/AdminLite/Layouts/Backend.php
@@ -199,7 +199,7 @@ $langMenuLinks = ob_get_clean();
                 <?php } ?>
                 </ul>
             </li>
-            <?php } else if ($item['url'] !== '#') { ?>
+            <?php } else { ?>
             <li <?= $item['active'] ? "class='active'" : ""; ?>>
                 <a href="<?= $item['url']; ?>"><i class="fa fa-<?= $item['icon'] ?>"></i> <span><?= $item['title']; ?></span></a>
             </li>

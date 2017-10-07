e1801704eadd6bd4e5f566d6f1c075914c467e3f
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-07 07:04:48 +0300

    Update the Language files

--- app/Modules/Files/Config/ElFinder.php
+++ app/Modules/Files/Config/ElFinder.php
@@ -13,7 +13,7 @@ return array(
 
     'roots' => array(
         array(
-            'alias'         => 'Site Assets',
+            'alias'         => __d('files', 'Site Assets'),
             'driver'        => 'LocalFileSystem',
             'path'          => ROOTDIR .'assets/',
             'URL'           => site_url('assets/'),
@@ -31,7 +31,7 @@ return array(
             'icon'          => site_url('modules/files/assets/img/volume_icon_local.png'),
         ),
         array(
-            'alias'         => 'Site Root',
+            'alias'         => __d('files', 'Site Root'),
             'driver'        => 'LocalFileSystem',
             'path'          => ROOTDIR,
             'URL'           => site_url('admin/files/preview/'),
--- app/Modules/Files/Language/RU/messages.php
+++ app/Modules/Files/Language/RU/messages.php
@@ -3,4 +3,6 @@
 return array (
   'Dashboard' => 'Рабочий стол',
   'Files' => 'Файлы',
+  'Site Assets' => '',
+  'Site Root' => '',
 );

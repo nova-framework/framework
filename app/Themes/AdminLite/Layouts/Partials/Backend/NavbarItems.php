1c5f6bc26b4dd61dd3d223a2aacc7b5a3b4cfedb
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-08 21:54:40 +0300

    Improve the AdminLite theme

---
+++ app/Themes/AdminLite/Layouts/Partials/Backend/NavbarItems.php
@@ -0,0 +1,17 @@
+<?php $children = Arr::get($item, 'children', array()); ?>
+<?php if (! empty($children)) { ?>
+<li class="dropdown <?= $item['active'] ? 'active' : ''; ?>">
+    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+        <i class="fa fa-<?= $item['icon'] ?>"></i> <span><?= $item['title']; ?></span> <span  class="caret"></span>
+    </a>
+    <ul class="dropdown-menu" role="menu">
+    <?php foreach ($children as $child) { ?>
+        <?= View::partial('Partials/Frontend/NavbarItems', 'AdminLite', array('item' => $child)); ?>
+    <?php } ?>
+    </ul>
+</li>
+<?php } else if ($item['url'] !== '#') { ?>
+<li <?= $item['active'] ? "class='active'" : ""; ?>>
+    <a href="<?= $item['url']; ?>"><i class="fa fa-<?= $item['icon'] ?>"></i> <span><?= $item['title']; ?></span></a>
+</li>
+<?php } ?>

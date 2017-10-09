983af2e126a8aaccf605d8dfe112c6da3043c696
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-09 22:45:50 +0300

    Overall improvements

--- app/Modules/System/Controllers/Notifications.php
+++ app/Modules/System/Controllers/Notifications.php
@@ -6,9 +6,9 @@ use Nova\Support\Facades\Auth;
 use Nova\Support\Facades\Input;
 use Nova\Support\Facades\Redirect;
 use Nova\Support\Facades\Response;
+use Nova\Support\Facades\Validator;
 
 use App\Modules\System\Controllers\BaseController;
-
 
 class Notifications extends BaseController
 {
@@ -28,7 +28,25 @@ class Notifications extends BaseController
     {
         $authUser = Auth::user();
 
-        return Redirect::back();
+        //
+        $input = Input::only('nid');
+
+        $validator = Validator::make($input,
+            array('nid' => 'required|array|exists:notifications,id'), array(), array('id' => 'ID')
+        );
+
+        if ($validator->fails()) {
+            return Redirect::back()->withInput()->withStatus($validator->errors(), 'danger');
+        }
+
+        $notifications = $authUser->notifications()->whereIn('id', $input['nid'])->get();
+
+        $notifications->markAsRead();
+
+        // Prepare the flash message.
+        $status = __d('system', 'The selected notification(s) was successfully marked as read.');
+
+        return Redirect::to('notifications')->withStatus($status);
     }
 
     public function data()

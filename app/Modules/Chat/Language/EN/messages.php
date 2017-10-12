91a94ccb13018fe5cd15d8ae6e6efd3fa575ea65
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-12 16:19:00 +0300

    Update the Chat module

--- app/Modules/Chat/Language/EN/messages.php
+++ app/Modules/Chat/Language/EN/messages.php
@@ -1,4 +1,14 @@
 <?php
 
 return array (
+  '%s (%s) joined the chat.' => '',
+  '%s (%s) left the chat.' => '',
+  '%s left chat.' => '',
+  'All' => '',
+  'Chat' => '',
+  'Dashboard' => '',
+  'On-line Users' => '',
+  'Send' => '',
+  'Type Message ...' => '',
+  'WebRTC Chat' => '',
 );

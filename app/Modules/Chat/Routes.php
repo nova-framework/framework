00127f93f1c973abe390538e89277786ee2cb1b0
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-12 15:50:59 +0300

    Initial implementation of the Chat module

---
+++ app/Modules/Chat/Routes.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Module Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for the module.
+| It's a breeze. Simply tell Nova the URIs it should respond to
+| and give it the Closure to execute when that URI is requested.
+|
+*/
+
+
+Route::get('chat', array('before' => 'auth', 'uses' => 'Chat@index'));
+

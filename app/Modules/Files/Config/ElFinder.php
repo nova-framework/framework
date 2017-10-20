bc78ac3427eb7b5061a19abaad8f58df7f9ef8a4
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-20 12:34:58 +0300

    Small changes

--- app/Modules/Files/Config/ElFinder.php
+++ app/Modules/Files/Config/ElFinder.php
@@ -6,6 +6,14 @@
  * @version 3.0
  */
 
+/**
+ * Simple function to demonstrate how to control file access using "accessControl" callback.
+ * This method will disable accessing files/folders starting from '.' (dot)
+ *
+ * @param  string  $attr  attribute name (read|write|locked|hidden)
+ * @param  string  $path  file path relative to volume root directory started with directory separator
+ * @return bool|null
+ **/
 
 $callback = function ($attr, $path, $data, $volume)
 {

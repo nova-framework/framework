35187db3898d5aac537dbc684c5770297fad0894
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-27 14:01:45 +0300

    Small changes

--- app/Console/Bootstrap.php
+++ app/Console/Bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Console - register the Forge Commands and Schedule
+ * Bootstrap - register the Forge Commands and Schedule
  *
  * @author Virgil-Adrian Teaca - virgil@giulianaeassociati.com
  * @version 3.0

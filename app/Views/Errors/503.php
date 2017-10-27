daf74d4def5ac8e817548dfc0a32f817532f344e
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-27 11:12:07 +0300

    Add the app/Views/Errors/503.php

---
+++ app/Views/Errors/503.php
@@ -0,0 +1,14 @@
+<div class="container content">
+    <div class="row">
+        <div class="col-md-12">
+
+            <h1>503</h1>
+
+            <hr />
+
+            <h3><?= __('Service unavailable'); ?></h3>
+            <p><?= __('Be right back.'); ?></p>
+
+        </div>
+    </div>
+</div>

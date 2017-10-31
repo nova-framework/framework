81202d464f3d319c6159b382535042b1bb1a2d23
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-10-31 12:50:01 +0200

    Small changes

--- app/Modules/Users/Views/Admin/Users/Search.php
+++ app/Modules/Users/Views/Admin/Users/Search.php
@@ -70,7 +70,7 @@
 
 </section>
 
-<?php if (count($deletables) > 0) { ?>
+<?php if ($deletables > 0) { ?>
 
 <div class="modal modal-default" id="modal-delete-dialog">
     <div class="modal-dialog">

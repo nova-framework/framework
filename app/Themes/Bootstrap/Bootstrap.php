2fec2b29d9d8f894aed007b1fce64eac17ea8e64
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2017-11-17 01:30:50 +0200

    Small changes

--- app/Themes/Bootstrap/Bootstrap.php
+++ app/Themes/Bootstrap/Bootstrap.php
@@ -9,6 +9,5 @@ Event::listen('content.editor.stylesheets.bootstrap', function ()
         vendor_url('dist/css/bootstrap.min.css', 'twbs/bootstrap'),
         vendor_url('dist/css/bootstrap-theme.min.css', 'twbs/bootstrap'),
         'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
-        resource_url('css/bootstrap-xl-mod.min.css'),
     );
 });

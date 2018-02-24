ddec35345359f528e51a77e4bdbfaa619124a8ad
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-02-24 20:49:54 +0200

    Overall improvements

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,13 @@
 
 namespace App\Providers;
 
+use Nova\Filesystem\FileNotFoundException;
 use Nova\Foundation\Support\Providers\AppServiceProvider as ServiceProvider;
 
 
 class AppServiceProvider extends ServiceProvider
 {
+
     /**
      * Bootstrap the Application Events.
      *
@@ -28,6 +30,46 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->registerInstalledPackages();
+
         //
+    }
+
+    /**
+     * Register the Application's Packages.
+     *
+     * @return void
+     */
+    protected function registerInstalledPackages()
+    {
+        $packages = $this->getInstalledPackages();
+
+        foreach ($packages as $package => $path) {
+            $namespace = str_replace('/', '\\', $package);
+
+            $provider = sprintf('%s\\Providers\\PackageServiceProvider', $namespace);
+
+            if (class_exists($provider)) {
+                $this->app->register($provider);
+            }
+        }
+    }
+
+    protected function getInstalledPackages()
+    {
+        $path = BASEPATH .'vendor' .DS .'nova-packages.php';
+
+        try {
+            $config = $this->app['files']->getRequire($path);
+        }
+        catch (FileNotFoundException $e) {
+            $config = array();
+        }
+
+        if (is_array($config) && isset($config['packages']) && is_array($config['packages'])) {
+            return $config['packages'];
+        }
+
+        return array();
     }
 }

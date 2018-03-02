6dcc8f07439f0a11b5259b90c6b672f5dfdc2700
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-03-02 17:22:28 +0200

    Small changes

--- app/Console.php
+++ app/Console.php
@@ -53,9 +53,9 @@ Forge::command('queue:monitor', function ()
 Schedule::command('queue:monitor')->everyMinute();
 
 // To prevent long running cache expiries it is advised to match your cache cache expiry time with your task frequency.
-//Schedule::command('queue:batch --tries=3 --time-limit=175 --job-limit=300')->everyMinute()->withoutOverlapping(3)->runInBackground();
+//Schedule::command('queue:batch --tries=3 --time-limit=175 --job-limit=300')->everyMinute()->withoutOverlapping(5)->runInBackground();
 
-//Schedule::command('queue:work --daemon')->everyFiveMinutes()->withoutOverlapping(15)->runInBackground();
+//Schedule::command('queue:work --daemon --tries=3')->everyFiveMinutes()->withoutOverlapping(15)->runInBackground();
 
 
 /**

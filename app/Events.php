9163f559fc155dfeca5e911aa278025d9965f9ee
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-03-06 14:42:11 +0200

    Improve the app/Events.php

--- app/Events.php
+++ app/Events.php
@@ -28,3 +28,21 @@ Event::listen('router.matched', function ($route, Request $request)
 
     View::share('currentUri', $request->path());
 });
+
+
+// Add a Listener to the Event 'nova.queue.looping', to check the database connection.
+Event::listen('nova.queue.looping', function ($connection, $queue)
+{
+    if ($connection != 'database') {
+        return;
+    }
+
+    try {
+        $count = DB::table('jobs')->count();
+
+        if ($count == 0) return false;
+    }
+    catch (Exception $e) {
+        return false;
+    }
+});

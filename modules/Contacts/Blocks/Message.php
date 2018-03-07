5fb2d41ba070ae7352d62405a08f4aa24f81adf2
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-03-07 17:03:21 +0200

    Improve the Contacts module

--- modules/Contacts/Blocks/Message.php
+++ modules/Contacts/Blocks/Message.php
@@ -2,23 +2,17 @@
 
 namespace Modules\Contacts\Blocks;
 
-use Nova\Support\Facades\App;
 use Nova\Support\Facades\Request;
 use Nova\Support\Facades\View;
 
 use Modules\Contacts\Models\Contact;
 use Modules\Content\Blocks\Block;
-use Modules\Content\Traits\ShortcodesTrait;
-
-use Thunder\Shortcode\Shortcode\ShortcodeInterface as Shortcode;
 
 use ErrorException;
 
 
 class Message extends Block
 {
-    use ShortcodesTrait;
-
 
     public function render()
     {
@@ -28,28 +22,6 @@ class Message extends Block
             throw new ErrorException('Contact not found');
         }
 
-        $this->addShortcode('input', function (Shortcode $shortcode)
-        {
-            return View::make('Modules/Contacts::Shortcodes/Input', compact('shortcode'));
-        });
-
-        $this->addShortcode('textarea', function (Shortcode $shortcode)
-        {
-            return View::make('Modules/Contacts::Shortcodes/Textarea', compact('shortcode'));
-        });
-
-        $this->addShortcode('select', function (Shortcode $shortcode)
-        {
-            return View::make('Modules/Contacts::Shortcodes/Select', compact('shortcode'));
-        });
-
-        $this->addShortcode('option', function (Shortcode $shortcode)
-        {
-            return View::make('Modules/Contacts::Shortcodes/Option', compact('shortcode'));
-        });
-
-        $content = $this->stripShortcodes($contact->message);
-
-        return View::make('Modules/Contacts::Blocks/Message', compact('path', 'content'))->render();
+        return View::make('Modules/Contacts::Blocks/Message', compact('contact', 'path'))->render();
     }
 }
--- modules/Contacts/Views/Blocks/Message.php
+++ modules/Contacts/Views/Blocks/Message.php
@@ -4,13 +4,54 @@
 <form action="<?= site_url('contacts'); ?>" method="POST">
 
 <input type="hidden" name="_token" value="<?= csrf_token(); ?>" />
+<input type="hidden" name="cid" value="<?= $contact->id; ?>" />
 <input type="hidden" name="path" value="<?= $path; ?>" />
 
 <div class="col-md-6 col-md-offset-1" style="margin-bottom: 50px;">
-<?= $content; ?>
-</form>
-
+    <div class="form-group<?= $errors->has('author') ? ' has-error' : ''; ?>">
+        <label class="control-label" for="author">
+            <?= __d('contacts', 'Name'); ?> <span class="text-danger" title="<?= __d('contacts', 'Required field'); ?>">*</span>
+        </label>
+        <input type="text" class="form-control" name="author" id="contact-form-author" placeholder="<?= __d('contacts', 'Name'); ?>" />
+        <div class="clearfix"></div>
+        <?php if ($errors->has('message')) { ?>
+        <span class="help-block">
+            <strong><?= $errors->first('author'); ?></strong>
+        </span>
+        <?php } ?>
+    </div>
+    <div class="form-group<?= $errors->has('author_email') ? ' has-error' : ''; ?>">
+        <label class="control-label" for="author_email">
+            <?= __d('contacts', 'E-mail Address'); ?> <span class="text-danger" title="<?= __d('contacts', 'Required field'); ?>">*</span>
+        </label>
+        <input type="text" class="form-control" name="author_email" id="contact-form-author-email" placeholder="<?= __d('contacts', 'E-mail Address'); ?>" />
+        <div class="clearfix"></div>
+        <?php if ($errors->has('author_email')) { ?>
+        <span class="help-block">
+            <strong><?= $errors->first('author_email'); ?></strong>
+        </span>
+        <?php } ?>
+    </div>
+    <div class="form-group<?= $errors->has('message') ? ' has-error' : '' ?>">
+        <label class="control-label" for="message">
+            <?= __d('contacts', 'Message'); ?> <span class="text-danger" title="<?= __d('contacts', 'Required field'); ?>">*</span>
+        </label>
+        <div class="clearfix"></div>
+        <div class="col-sm-12" style="padding: 0;">
+            <textarea name="message" id="contact-form-message" rows="10" class="form-control" style="resize: none;" placeholder="<?= __d('contacts', 'Message'); ?>"><?= Input::old('message'); ?></textarea>
+        </div>
+        <div class="clearfix"></div>
+        <?php if ($errors->has('message')) { ?>
+        <span class="help-block">
+            <strong><?= $errors->first('message'); ?></strong>
+        </span>
+        <?php } ?>
+    </div>
+    <div class="form-group">
+        <input type="submit" name="submit" class="btn btn-primary col-md-3 pull-right" value="<?= __d('contacts', 'Submit'); ?>" />
+    </div>
 </div>
 
-<div class="clear"></div>
+</form>
 
+<div class="clear"></div>

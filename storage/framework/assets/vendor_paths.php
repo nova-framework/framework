0fc36ed82246cc692c5aff6f99feb4c530179e64
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-03-22 05:24:44 +0200

    Remove stalled cache file

--- storage/framework/assets/vendor_paths.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array (
-  0 => 'almasaeed2010/adminlte/bower_components/',
-  1 => 'almasaeed2010/adminlte/dist/',
-  2 => 'almasaeed2010/adminlte/plugins/',
-  3 => 'twbs/bootstrap/dist/',
-);

4fd71c7885c2ef107a1a4c2ba60444dc94b878fb
Victor Efremov <victor.efremov@gmail.com>
2018-03-22 23:37:13 +0300

    [4.x] update the language (RU)

--- modules/Permissions/Language/RU/messages.php
+++ modules/Permissions/Language/RU/messages.php
@@ -6,9 +6,9 @@ return array (
   'Permission' => 'Разрешение',
   'Permissions' => 'Разрешения',
   'Permissions registered by the <b>{0}</b> package' => 'Разрешения, зарегистрированные для пакета <b>{0}</b>',
-  'Permissions registered by the main <b>Application</b>' => 'Разрешения, зарегистрированные <b>Приложением</b>',
+  'Permissions registered by the main <b>Application</b>' => 'Разрешения, зарегистрированные для <b>Приложения</b>',
   'Platform' => 'Платформа',
   'Registered Permissions' => 'Зарегистрированные разрешения',
-  'The main <b>Application</b> has no registered permissions.' => 'В главном <b>Приложении</b> нет зарегистрированных разрешений.',
+  'The main <b>Application</b> has no registered permissions.' => 'В основном <b>Приложении</b> нет зарегистрированных разрешений.',
   'The permissions was successfully updated.' => 'Разрешения были успешно обновлены.',
 );

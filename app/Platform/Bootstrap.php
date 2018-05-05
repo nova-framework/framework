1cb7d6868f7a1bc9b225c9d949638b080132c606
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-05-05 14:10:25 +0300

    Update the application version

--- app/Platform/Bootstrap.php
+++ app/Platform/Bootstrap.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 // Define The Application Version
 //--------------------------------------------------------------------------
 
-define('VERSION', '4.0.57');
+define('VERSION', '4.0.58');
 
 //--------------------------------------------------------------------------
 // Set PHP Error Reporting Options

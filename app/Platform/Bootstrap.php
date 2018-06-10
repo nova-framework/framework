653deb94501e6cd5fc06002f11afa82a17f7c5bb
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-06-10 22:19:34 +0300

    Improve the Contacts module

--- app/Platform/Bootstrap.php
+++ app/Platform/Bootstrap.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 // Define The Application Version
 //--------------------------------------------------------------------------
 
-define('VERSION', '4.0.72');
+define('VERSION', '4.0.73');
 
 //--------------------------------------------------------------------------
 // Set PHP Error Reporting Options

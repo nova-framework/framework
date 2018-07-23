58f07c22a28d4e697f4b9dbb232d44f341247c34
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2018-07-23 03:33:47 +0300

    Overall improvements

--- app/Platform/Bootstrap.php
+++ app/Platform/Bootstrap.php
@@ -14,7 +14,7 @@ use Symfony\Component\HttpFoundation\BinaryFileResponse;
 // Define The Application Version
 //--------------------------------------------------------------------------
 
-define('VERSION', '4.0.75');
+define('VERSION', '4.0.76');
 
 //--------------------------------------------------------------------------
 // Set PHP Error Reporting Options

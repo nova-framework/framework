66c2eb82056da1ea28c79085681b871d1462ddcf
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2019-04-13 16:24:35 +0300

    Remove some debug code

--- modules/Content/Views/Partials/TaxonomyCheckBox.php
+++ modules/Content/Views/Partials/TaxonomyCheckBox.php
@@ -1,5 +1,3 @@
-<?php dd(get_defined_vars()); ?>
-
 <div class="checkbox">
     <label>
         <input class="<?= $type; ?>-checkbox" name="<?= $type; ?>[]" value="<?= $taxonomy->id; ?>" type="checkbox" <?= in_array($taxonomy->id, $selected) ? ' checked="checked"' : ''; ?>>

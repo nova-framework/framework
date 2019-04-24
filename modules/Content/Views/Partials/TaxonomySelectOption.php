9794c50f628cb6afbd1969c25e4f1ffc4c4d68e7
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2019-04-24 09:11:17 +0300

    Improve the Content module

--- modules/Content/Views/Partials/TaxonomySelectOption.php
+++ modules/Content/Views/Partials/TaxonomySelectOption.php
@@ -1,3 +1,2 @@
-<?php $padding = str_repeat('--', $level); ?>
-<?php $name = isset($taxonomy) ? e($taxonomy->name) : __d('content', 'None'); ?>
-<option value="<?= $itemId = isset($taxonomy) ? $taxonomy->id : 0; ?>"<?= ($itemId == $parentId) ? ' selected="selected"' : ''; ?>><?= trim($padding .' ' .$name); ?></option>
+<?php $name = isset($taxonomy) ? trim(str_repeat('--', $level) .' ' .e($taxonomy->name)) : __d('content', 'None'); ?>
+<option value="<?= $itemId = isset($taxonomy) ? $taxonomy->id : 0; ?>"<?= ($itemId == $parentId) ? ' selected="selected"' : ''; ?>><?= $name; ?></option>

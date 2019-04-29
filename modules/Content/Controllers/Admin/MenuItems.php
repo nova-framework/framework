fcdb9cd4b13963286175bac89cfd48966e4dca2d
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2019-04-29 08:50:47 +0300

    Improve the Content module

--- modules/Content/Controllers/Admin/MenuItems.php
+++ modules/Content/Controllers/Admin/MenuItems.php
@@ -8,7 +8,6 @@ use Nova\Database\ORM\ModelNotFoundException;
 use Nova\Http\Request;
 use Nova\Support\Facades\Auth;
 use Nova\Support\Facades\Cache;
-use Nova\Support\Facades\Event;
 use Nova\Support\Facades\Redirect;
 use Nova\Support\Facades\Validator;
 use Nova\Support\Facades\View;

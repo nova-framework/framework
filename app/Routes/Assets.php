57d37bb5a2d9e5b61449fccae86fb660a29237e0
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2019-07-13 23:25:09 +0300

    Improve the routes

--- app/Routes/Assets.php
+++ app/Routes/Assets.php
@@ -14,19 +14,21 @@ $dispatcher->route('assets/(:all)', function (Request $request, $path) use ($dis
 // Register the route for assets from Packages, Modules and Themes.
 $dispatcher->route('packages/(:any)/(:any)/(:all)', function (Request $request, $vendor, $package, $path) use ($dispatcher)
 {
-    if (is_null($packagePath = $dispatcher->getPackagePath($vendor, $package))) {
+    if (empty($basePath = $dispatcher->getPackagePath($vendor, $package))) {
         return Response::make('File Not Found', 404);
     }
 
-    return $packagePath .str_replace('/', DS, $path);
+    return $basePath .DS .str_replace('/', DS, $path);
 });
 
 // Register the route for assets from Vendor.
 $dispatcher->route('vendor/(:all)', function (Request $request, $path) use ($dispatcher)
 {
+    $basePath = BASEPATH .'vendor' .DS;
+
     if (! Str::startsWith($path, $dispatcher->getVendorPaths())) {
         return Response::make('File Not Found', 404);
     }
 
-    return BASEPATH .'vendor' .DS .str_replace('/', DS, $path);
+    return $basePath .DS .str_replace('/', DS, $path);
 });

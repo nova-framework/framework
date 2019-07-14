eb0f3463cef88a84adfa64bf56a946083833c191
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2019-07-14 07:45:41 +0300

    Improve the routes

--- app/Routes/Assets.php
+++ app/Routes/Assets.php
@@ -8,27 +8,19 @@ $dispatcher->route('assets/(:all)', function (Request $request, $path) use ($dis
 {
     $basePath = Config::get('routing.assets.path', BASEPATH .'assets');
 
-    return $basePath .DS .str_replace('/', DS, $path);
+    $path = $basePath .DS .str_replace('/', DS, $path);
+
+    return $dispatcher->serve($path, $request);
 });
 
 // Register the route for assets from Packages, Modules and Themes.
 $dispatcher->route('packages/(:any)/(:any)/(:all)', function (Request $request, $vendor, $package, $path) use ($dispatcher)
 {
-    if (empty($basePath = $dispatcher->getPackagePath($vendor, $package))) {
-        return Response::make('File Not Found', 404);
-    }
-
-    return $basePath .DS .str_replace('/', DS, $path);
+    return $dispatcher->servePackageFile($vendor, $package, $path, $request);
 });
 
 // Register the route for assets from Vendor.
 $dispatcher->route('vendor/(:all)', function (Request $request, $path) use ($dispatcher)
 {
-    $basePath = BASEPATH .'vendor';
-
-    if (! Str::startsWith($path, $dispatcher->getVendorPaths())) {
-        return Response::make('File Not Found', 404);
-    }
-
-    return $basePath .DS .str_replace('/', DS, $path);
+    return $dispatcher->serveVendorFile($path, $request);
 });

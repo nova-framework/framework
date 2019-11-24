32cd0a9c068193ab506ca3406e31a3bab98140eb
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2019-11-24 19:03:38 +0200

    Improve the App\Providers\AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,19 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $session = $this->app['session'];
-
-        if (! $session->has('language')) {
-            $cookie = $this->app['request']->cookie(PREFIX .'language', null);
-
-            $locale = $cookie ?: $this->app['config']->get('app.locale');
-
-            $session->set('language', $locale);
-        } else {
-            $locale = $session->get('language');
-        }
-
-        $this->app['language']->setLocale($locale);
+        //
     }
 
     /**

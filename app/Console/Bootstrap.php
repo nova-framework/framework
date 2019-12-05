97baebc2cb500d8f0d67793db3c24a2e2da4141f
Virgil-Adrian Teaca <darkstarlinux@gmail.com>
2019-12-05 22:37:52 +0200

    Improve the console support

--- app/Console/Bootstrap.php
+++ app/Console/Bootstrap.php
@@ -17,29 +17,43 @@ Forge::resolveCommands(array(
 /**
  * Add the Closure based commands.
  */
+Forge::command('app:install', function ()
+{
+    $this->call('migrate', array('--seed' => true));
+
+    //
+    $this->call('package:migrate');
+    $this->call('package:seed');
+
+})->describe('Run all database migrations and seed it with records');
+
+Forge::command('app:refresh', function ()
+{
+    $this->call('migrate:refresh', array('--seed' => true));
+
+    //
+    $this->call('package:migrate:reset');
+    $this->call('package:migrate');
+    $this->call('package:seed');
+
+})->describe('Reset and re-run all database migrations, then seed it with records');
+
 Forge::command('queue:monitor', function ()
 {
-    $runCommand = true;
-
-    // Check if the Queue Worker is already running.
     $path = storage_path('queue.pid');
 
     if (is_readable($path) && ! empty($pid = file_get_contents($path))) {
-        $command = "ps -p {$pid} --no-heading | awk '{print $1}'";
+        $command = sprintf("ps -p %d --no-heading | awk '{print $1}'", $pid);
 
-        if (! empty($result = exec($command))) {
-            $runCommand = false;
+        if (! empty($result = exec($command)) && ($result == $pid)) {
+            return;
         }
     }
 
-    if ($runCommand) {
-        $command = sprintf('%s %s queue:work --daemon --tries=3 >/dev/null & echo $!', PHP_BINARY, base_path('forge'));
+    $command = sprintf('%s %s queue:work --daemon --tries=3 >/dev/null & echo $!', PHP_BINARY, base_path('forge'));
 
-        $pid = exec($command);
-
-        // Store the Queue Worker PID for later checking.
-        file_put_contents($path, $pid);
-    }
+    // Store the Queue Worker PID for later checking.
+    file_put_contents($path, exec($command));
 
 })->describe('Monitor the Queue Worker execution');
 
